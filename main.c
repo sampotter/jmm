68b72f2714b49f6994efacae54995239b28f304b
Samuel F. Potter <sfp@umiacs.umd.edu>
2019-09-27 21:12:52 -0400

    add command line arguments

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <math.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define EPS 1e-13
 #define SQRT2 1.414213562373095
@@ -674,20 +676,90 @@ dvec2 df(dvec2 p) {
   return v;
 }
 
-int main() {
-  int m = 51, n = 31;
-  dbl h = 1.0/(n - 1);
-  dbl rf = 0.1;
+void usage() {
+  printf("hello\n");
+}
 
-  ivec2 ind0 = {m/2, n/2};
+int main(int argc, char *argv[]) {
+  int m = 101, n = 101, i = -1, j = -1;
+  double h = -1, r = 0.1;
+  char *path = NULL;
+  bool verbose = false;
 
-  ivec2 shape = {.i = m, .j = n};
+  char c;
+  while ((c = getopt(argc, argv, "m:n:i:j:h:r:o:vH")) != -1) {
+    switch (c) {
+    case 'm':
+      m = atoi(optarg);
+      break;
+    case 'n':
+      n = atoi(optarg);
+      break;
+    case 'i':
+      i = atoi(optarg);
+      break;
+    case 'j':
+      j = atoi(optarg);
+      break;
+    case 'h':
+      h = atof(optarg);
+      break;
+    case 'r':
+      r = atof(optarg);
+      break;
+    case 'o':
+      path = malloc(strlen(optarg) + 1);
+      strcpy(path, optarg);
+      break;
+    case 'v':
+      verbose = true;
+      break;
+    case 'H':
+      usage();
+      exit(EXIT_SUCCESS);
+    }
+  }
+
+  if (i == -1) {
+    i = m/2;
+  }
+  if (j == -1) {
+    j = n/2;
+  }
+  if (h == -1) {
+    h = 1.0/(m - 1);
+  }
+
+  if (verbose) {
+    printf("(m, n) = (%d, %d), ", m, n);
+    printf("h = %g, ", h);
+    printf("(i, j) = (%d, %d), ", i, j);
+    printf("r = %g, ", r);
+    printf("path: %s\n", path ? path : "A.bin");
+  }
+
+  ivec2 ind = {i, j};
+  ivec2 shape = {m, n};
   func s = {.f = f, .df = df};
 
   sjs_s sjs;
   sjs_init(&sjs, shape, h, &s);
-  sjs_add_fac_pt_src(&sjs, ind0, rf);
+  sjs_add_fac_pt_src(&sjs, ind, r);
   sjs_solve(&sjs);
+
+  FILE *f = fopen(path ? path : "A.bin", "w");
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0, l; j < n; ++j) {
+      ivec2 ind = {i, j};
+      l = sjs_lindex(&sjs, ind);
+      fwrite(sjs.bicubics[l].A, sizeof(dbl), 16, f);
+    }
+  }
+  fclose(f);
+
+  if (path != NULL) {
+    free(path);
+  }
 
   free(sjs.bicubics);
   free(sjs.jets);
