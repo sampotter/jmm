8d13228bf8c3f6615b6661e393de1d50dd6706a2
Samuel F. Potter <sfp@umiacs.umd.edu>
2020-02-26 17:39:46 -0500

    trash Cython stuff

--- setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup, Extension
-from glob import glob
-from Cython.Build import cythonize
-
-ext = Extension('pysjs', ['sjs.pyx'], libraries=['sjs_eikonal'])
-
-setup(name='pysjs', ext_modules=cythonize([ext]))
