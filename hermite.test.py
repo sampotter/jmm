fe22481c878f7ab374f26dc4fc0da232a3505858
Samuel Potter <sfp@umiacs.umd.edu>
2020-02-27 12:50:58 -0500

    start adding tests

---
+++ hermite.test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import sjs_eik
+import unittest
+
+class TestCubic(unittest.TestCase):
+
+    def test_ctor(self):
+        for _ in range(10):
+            data = np.random.randn(4)
+            cubic = sjs_eik.Cubic(data)
+            self.assertTrue(np.isclose(cubic.f(0), data[0]))
+            self.assertTrue(np.isclose(cubic.f(1), data[1]))
+            self.assertTrue(np.isclose(cubic.df(0), data[2]))
+            self.assertTrue(np.isclose(cubic.df(1), data[3]))
+
+if __name__ == '__main__':
+    unittest.main()
