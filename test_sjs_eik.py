eccd41ef96c3913b9838ae9aaacb341d4fc8a698
Samuel Potter <sfp@umiacs.umd.edu>
2020-02-29 19:57:43 -0500

    add start of test_sjs_eik.py

---
+++ test_sjs_eik.py
@@ -0,0 +1,26 @@
+import numpy as np
+import sjs_eik as sjs
+import unittest
+
+class TestStaticJetScheme(unittest.TestCase):
+
+    def test_set_jet(self):
+        shape = (2, 2)
+        xymin = (0, 0)
+        h = 1
+        s = lambda x, y: 1.0 + 0.0*x + 0.0*y
+        grad_s = lambda x, y: 0.0 + 0.0*x + 0.0*y
+        scheme = sjs.StaticJetScheme(shape, xymin, h, s, grad_s)
+        scheme.set_jet(0, 0, sjs.Jet(1, 0, 0, 0))
+        scheme.set_jet(1, 0, sjs.Jet(1, 0, 0, 0))
+        scheme.set_jet(0, 1, sjs.Jet(1, 0, 0, 0))
+        scheme.set_jet(1, 1, sjs.Jet(1, 0, 0, 0))
+        for _ in range(100):
+            x, y = np.random.rand(2)
+            self.assertAlmostEqual(scheme.T(x, y), 1.0)
+            self.assertAlmostEqual(scheme.Tx(x, y), 0.0)
+            self.assertAlmostEqual(scheme.Ty(x, y), 0.0)
+            self.assertAlmostEqual(scheme.Txy(x, y), 0.0)
+
+if __name__ == '__main__':
+    unittest.main()
