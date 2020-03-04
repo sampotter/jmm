fd7350110888cd544055e1120703d800c28e1511
Samuel F. Potter <sfp@umiacs.umd.edu>
2020-03-04 14:07:20 -0500

    fix bugs in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "sjs_eik.h"
 
 #include <stdlib.h>
+#include <stdio.h>
+
+#define MAX(x, y) x > y ? x : y
 
 dbl T(dbl x, dbl y) {
   return sqrt(x*x + y*y);
@@ -26,7 +29,7 @@ int main() {
   int i0 = N/2;
   ivec2 shape = {N, N};
   dvec2 xymin = {-1, -1};
-  dbl h = 2/(N-1);
+  dbl h = 2.0/(N-1);
   sjs_init(scheme, shape, xymin, h);
 
   int R = 5;
@@ -36,7 +39,7 @@ int main() {
     for (int j = 0; j < N; ++j) {
       int abs_dj = abs(j - i0);
       dbl y = h*j + xymin.y;
-      int r = abs_di + abs_dj;
+      int r = MAX(abs_di, abs_dj);
       if (r <= R) {
         ivec2 ind = {i, j};
         jet J = {T(x, y), Tx(x, y), Ty(x, y), Txy(x, y)};
