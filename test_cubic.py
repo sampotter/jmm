75cb5e40a006830e02b76f54c325bb5bb3b5da1c
Samuel F. Potter <sfp@umiacs.umd.edu>
2020-03-04 14:06:49 -0500

    use assertAlmostEqual

--- test_cubic.py
+++ test_cubic.py
@@ -8,10 +8,10 @@ class TestCubic(unittest.TestCase):
         for _ in range(10):
             data = np.random.randn(4)
             cubic = sjs_eik.Cubic(data)
-            self.assertTrue(np.isclose(cubic.f(0), data[0]))
-            self.assertTrue(np.isclose(cubic.f(1), data[1]))
-            self.assertTrue(np.isclose(cubic.df(0), data[2]))
-            self.assertTrue(np.isclose(cubic.df(1), data[3]))
+            self.assertAlmostEqual(cubic.f(0), data[0])
+            self.assertAlmostEqual(cubic.f(1), data[1])
+            self.assertAlmostEqual(cubic.df(0), data[2])
+            self.assertAlmostEqual(cubic.df(1), data[3])
 
     def test_reverse_on_unit_interval(self):
         for _ in range(10):
