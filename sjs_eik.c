6084cb579ca5bbef854abb6ca437f126fa2c12e2
Samuel F. Potter <sfp@umiacs.umd.edu>
2020-03-10 11:59:27 -0400

    rewrite of some major sjs_eik.c stuff

--- sjs_eik.c
+++ sjs_eik.c
@@ -10,26 +10,31 @@
 #include "jet.h"
 #include "update.h"
 
+#define NUM_CELL_VERTS 4
+#define NUM_CELL_NB_VERTS 9
+#define NUM_NB 8
+#define NUM_NB_CELLS 4
+#define NUM_NEARBY_CELLS 16
+
 /**
+ * TODO: add a few words about what `sjs` is and how it works
+ *
  * TODO: add a few comments about how indexing works
  *
  * - row-major ordering is used
  * - l vs lc index spaces
+ * - cell verts are in column major order
  */
-
-#define NUM_CELL_VERTS 4
-#define NUM_NB 8
-#define NUM_NEARBY_CELLS 16
-
 struct sjs {
   ivec2 shape;
   dvec2 xymin;
   dbl h;
   int nnodes, ncells;
   int nb_dl[NUM_NB + 1];
+  int cell_nb_verts_dl[NUM_CELL_NB_VERTS];
   int vert_dl[NUM_CELL_VERTS];
   int tri_dlc[NUM_NB];
-  int nb_dlc[NUM_CELL_VERTS];
+  int nb_dlc[NUM_NB_CELLS];
   int nearby_dlc[NUM_NEARBY_CELLS];
   bicubic_s *bicubics;
   jet_s *jets;
@@ -38,6 +43,13 @@ struct sjs {
   heap_s *heap;
 };
 
+/**
+ * This array maps a linear indexing of neighbors surrounding a grid
+ * node to local (i, j) offsets. There are NUM_NB + 1 = 9 entries
+ * instead of 8 so that it's not necessary to use modular arithmetic
+ * to access the offsets at i +/- 1 in a for loop (this is nice for
+ * triangle updates).
+ */
 static ivec2 offsets[NUM_NB + 1] = {
   {.i = -1, .j = -1},
   {.i = -1, .j =  0},
@@ -107,13 +119,11 @@ static ivec2 tri_cell_offsets[NUM_NB] = {
   {.i = -1, .j = -2}
 };
 
-static ivec2 nb_cell_offsets[NUM_CELL_VERTS] = {
-  {.i = -1, .j = -1},
-  {.i =  0, .j = -1},
-  {.i = -1, .j =  0},
-  {.i =  0, .j =  0}
-};
-
+/**
+ * TODO: document this
+ *
+ * - used to set nearby_dlc
+ */
 static ivec2 nearby_cell_offsets[NUM_NEARBY_CELLS] = {
   {.i = -2, .j = -2}, {.i = -2, .j = -1}, {.i = -2, .j = 0}, {.i = -2, .j = 1},
   {.i = -1, .j = -2}, {.i = -1, .j = -1}, {.i = -1, .j = 0}, {.i = -1, .j = 1},
@@ -133,34 +143,56 @@ static bool should_reverse_cubic[NUM_NB] = {
   /* ic0 -> */ true, false, true, false, false, true, false, true
 };
 
-/**
- * TODO: to switch this from the column major ordering using by
- * `cell_vert_offsets` (how it is now), just swap the middle two
- * columns below
- */
 #define _ NO_INDEX
-static int dirty2updated[NUM_NEARBY_CELLS][NUM_CELL_VERTS - 1] = {
-  {0, _, _}, {0, 1, _}, {1, 2, _}, {2, _, _},
-  {0, 7, _}, {0, 1, 7}, {1, 2, 3}, {2, 3, _},
-  {6, 7, _}, {5, 6, 7}, {3, 4, 5}, {3, 4, _},
-  {6, _, _}, {5, 6, _}, {4, 5, _}, {4, _, _}
+static int cell_verts_to_cell_nb_verts[NUM_NEARBY_CELLS][NUM_CELL_VERTS] = {
+  {_, _, _, 0}, {_, 0, _, 1}, {_, 1, _, 2}, {_, 2, _, _},
+  {_, _, 0, 3}, {0, 3, 1, 4}, {1, 4, 2, 5}, {2, 5, _, _},
+  {_, _, 3, 6}, {3, 6, 4, 7}, {4, 7, 5, 8}, {5, 8, _, _},
+  {_, _, 6, _}, {6, _, 7, _}, {7, _, 8, _}, {8, _, _, _}
 };
 #undef _
 
-static int updated2dirty[NUM_NB][NUM_CELL_VERTS] = {
-  {0, 1, 4, 5},
-  {1, 2, 5, 6},
-  {2, 3, 6, 7},
-  {6, 7, 10, 11},
-  {10, 11, 14, 15},
-  {9, 10, 13, 14},
-  {8, 9, 12, 13},
-  {4, 5, 8, 9}
+#define _ NO_INDEX
+static int cell_nb_verts_to_nearby_cells[NUM_CELL_NB_VERTS][NUM_NB_CELLS] = {
+  { 5,  1,  4,  0},
+  { 6,  2,  5,  1},
+  { 7,  3,  6,  2},
+  { 9,  5,  8,  4},
+  {10,  6,  9,  5},
+  {11,  7, 10,  6},
+  {13,  9, 12,  8},
+  {14, 10, 13,  9},
+  {15, 11, 14, 10}
+};
+#undef _
+
+static bool nearby_cell_is_cell_nb[NUM_NEARBY_CELLS] = {
+  false, false, false, false,
+  false,  true,  true, false,
+  false,  true,  true, false,
+  false, false, false, false
 };
 
 static void set_nb_dl(sjs_s *sjs) {
   for (int i = 0; i < NUM_NB + 1; ++i) {
     sjs->nb_dl[i] = ind2l(sjs->shape, offsets[i]);
+  }
+}
+
+static void set_cell_nb_verts_dl(sjs_s *sjs) {
+  static ivec2 cell_nb_verts_offsets[NUM_CELL_NB_VERTS] = {
+    {.i = -1, .j = -1},
+    {.i = -1, .j =  0},
+    {.i = -1, .j =  1},
+    {.i =  0, .j = -1},
+    {.i =  0, .j =  0},
+    {.i =  0, .j =  1},
+    {.i =  1, .j = -1},
+    {.i =  1, .j =  0},
+    {.i =  1, .j =  1}
+  };
+  for (int i = 0; i < NUM_CELL_NB_VERTS; ++i) {
+    sjs->cell_nb_verts_dl[i] = ind2l(sjs->shape, cell_nb_verts_offsets[i]);
   }
 }
 
@@ -177,6 +209,16 @@ static void set_tri_dlc(sjs_s *sjs) {
 }
 
 static void set_nb_dlc(sjs_s *sjs) {
+  /**
+   * This array gives the offsets ind "indc" space from the linear
+   * index of a grid node to its four neighboring cells.
+   */
+  static ivec2 nb_cell_offsets[NUM_NB_CELLS] = {
+    {.i = -1, .j = -1},
+    {.i =  0, .j = -1},
+    {.i = -1, .j =  0},
+    {.i =  0, .j =  0}
+  };
   for (int i = 0; i < NUM_CELL_VERTS; ++i) {
     sjs->nb_dlc[i] = ind2lc(sjs->shape, nb_cell_offsets[i]);
   }
@@ -197,7 +239,7 @@ static dvec2 get_xy(sjs_s *sjs, int l) {
   return xy;
 }
 
-static bool line(sjs_s *sjs, int l, int l0) {
+static void line(sjs_s *sjs, int l, int l0) {
   dvec2 xy = get_xy(sjs, l);
   dvec2 xy0 = get_xy(sjs, l0);
   dvec2 xy0_minus_xy = dvec2_sub(xy0, xy);
@@ -206,35 +248,38 @@ static bool line(sjs_s *sjs, int l, int l0) {
   dbl T0 = sjs->jets[l0].f;
   dbl T = T0 + L;
 
-  jet_s *J = &sjs->jets[l];
-  bool updated = false;
-  if (T < J->f) {
-    updated = true;
-    J->f = T;
-    J->fx = -xy0_minus_xy.x/L;
-    J->fy = -xy0_minus_xy.y/L;
+  // Check causality
+  assert(T > sjs->jets[l0].f);
+
+  jet_s *jet = &sjs->jets[l];
+  if (T < jet->f) {
+    jet->f = T;
+    jet->fx = -xy0_minus_xy.x/L;
+    jet->fy = -xy0_minus_xy.y/L;
   }
-  return updated;
 }
 
 /**
  * In this function, `ic0` is used as an index to select a nearby
- * bicubic interpolant which will be used to approximate `T` locally.
+ * bicubic interpolant which will be used to approximate `T`
+ * locally.
+ *
+ * If the cell being indexed by ic0 is out of bounds, or if the cell
+ * is invalid, this function does nothing.
  */
-static bool tri(sjs_s *sjs, int l, int l0, int l1, int ic0) {
+static void tri(sjs_s *sjs, int l, int l0, int l1, int ic0) {
   assert(ic0 >= 0);
   assert(ic0 < NUM_NB);
 
   int lc = l2lc(sjs->shape, l) + sjs->tri_dlc[ic0];
   if (lc < 0 || sjs->ncells <= lc) {
-    return false;
+    return;
   }
 
   bicubic_s *bicubic = &sjs->bicubics[lc];
-#if SJS_DEBUG
-  assert(bicubic_valid(bicubic));
-#endif
-  // TODO: assert valid state
+  if (!bicubic_valid(bicubic)) {
+    return;
+  }
 
   /**
    * Get cubic along edge of interest.
@@ -257,22 +302,20 @@ static bool tri(sjs_s *sjs, int l, int l0, int l1, int ic0) {
   dbl lam = hybrid(dF_dt, 0, 1, context);
   dbl T = F(lam, context);
 
-  jet_s *J = &sjs->jets[l];
+  // Check causality
+  assert(T > sjs->jets[l0].f);
+  assert(T > sjs->jets[l1].f);
 
-  bool updated = false;
-  if (T < J->f) {
-    updated = true;
-
+  jet_s *jet = &sjs->jets[l];
+  if (T < jet->f) {
     dvec2 xy = get_xy(sjs, l);
     dvec2 xylam = dvec2_ccomb(data.xy0, data.xy1, lam);
     dvec2 xylam_minus_xy = dvec2_sub(xylam, xy);
     dbl L = dvec2_norm(xylam_minus_xy);
-
-    J->f = T;
-    J->fx = -xylam_minus_xy.x/L;
-    J->fy = -xylam_minus_xy.y/L;
+    jet->f = T;
+    jet->fx = -xylam_minus_xy.x/L;
+    jet->fy = -xylam_minus_xy.y/L;
   }
-  return updated;
 }
 
 /**
@@ -295,14 +338,55 @@ static bool can_build_cell(sjs_s const *sjs, int lc) {
      * TODO: we don't want to build cells that only have trial values,
      * I don't think...
      */
-    if (state != TRIAL && state != VALID) {
+    if (state != VALID) {
       return false;
     }
   }
   return true;
 }
 
-static void est_Txy_values(sjs_s *sjs, int lc, dbl Txy[4]) {
+/**
+ * TODO: we should describe precisely what "valid" means for a
+ * cell. Right now or definition is just "the cell is inbounds and all
+ * of its incident vertices are VALID". If all of the incident
+ * vertices are VALID, then T, Tx, and Ty should all be finite for
+ * each incident vertex, which is enough to bilinearly interpolate Txy
+ * values.
+ */
+static bool cell_is_valid(sjs_s const *sjs, int lc) {
+  if (!(0 <= lc && lc <= sjs->ncells)) {
+    return false;
+  }
+  int l = lc2l(sjs->shape, lc);
+  for (int iv = 0, lv; iv < NUM_CELL_VERTS; ++iv) {
+    lv = l + sjs->vert_dl[iv];
+    if (sjs->states[lv] != VALID) {
+      return false;
+    }
+  }
+  return true;
+}
+
+static dvec4 interpolate_Txy_at_verts(sjs_s *sjs, int lc) {
+  /**
+   * TODO: this probably works, but we'll use the original thing just
+   * to be safe... Once we're computing good `fxy` values we can turn
+   * this back on
+   */
+  // dvec4 fx, fy;
+  // int l = lc2l(sjs->shape, lc);
+  // for (int iv = 0, lv; iv < NUM_CELL_VERTS; ++iv) {
+  //   lv = l + sjs->vert_dl[iv];
+  //   fx.data[iv] = sjs->jets[lv].fx;
+  //   fy.data[iv] = sjs->jets[lv].fy;
+  // }
+  // return interpolate_fxy_at_verts(fx, fy, sjs->h);
+
+  /**
+   * ... we'll just use this for now since I have more confidence that
+   * it *does* work
+   */
+
   dbl fx[NUM_CELL_VERTS], fy[NUM_CELL_VERTS];
 
   for (int i = 0, l; i < NUM_CELL_VERTS; ++i) {
@@ -323,12 +407,26 @@ static void est_Txy_values(sjs_s *sjs, int lc, dbl Txy[4]) {
 
   dbl lam, mu;
 
+  dvec4 Txy;
+
   for (int i = 0; i < 4; ++i) {
     lam = lams[i];
     mu = mus[i];
-    Txy[i] = (1 - mu)*((1 - lam)*fxy[0] + lam*fxy[1]) +
+    Txy.data[i] = (1 - mu)*((1 - lam)*fxy[0] + lam*fxy[1]) +
       mu*((1 - lam)*fxy[2] + lam*fxy[3]);
   }
+
+  return Txy;
+}
+
+static dvec4 get_cell_Txy_values(sjs_s const *sjs, int lc) {
+  dvec4 Txy;
+  for (int i = 0, l; i < NUM_CELL_VERTS; ++i) {
+    l = lc2l(sjs->shape, lc) + sjs->vert_dl[i];
+    Txy.data[i] = sjs->jets[l].fxy;
+    assert(isfinite(Txy.data[i]));
+  }
+  return Txy;
 }
 
 /**
@@ -377,36 +475,32 @@ static void build_cell(sjs_s *sjs, int lc) {
   bicubic_set_data(&sjs->bicubics[lc], data);
 }
 
-static bool update(sjs_s *sjs, int l) {
-  bool updated = false;
-
+static void update(sjs_s *sjs, int l) {
   for (int i0 = 1, l0, l1, ic0; i0 < 8; i0 += 2) {
     l0 = l + sjs->nb_dl[i0];
-    if (inbounds(sjs, l0) && sjs->states[l0] != VALID) {
+    if (!inbounds(sjs, l0) || sjs->states[l0] != VALID) {
       continue;
     }
 
     l1 = l + sjs->nb_dl[i0 - 1];
     if (inbounds(sjs, l1) && sjs->states[l1] == VALID) {
       ic0 = i0 - 1;
-      updated |= tri(sjs, l, l0, l1, ic0);
+      tri(sjs, l, l0, l1, ic0);
     }
 
     l1 = l + sjs->nb_dl[i0 + 1];
     if (inbounds(sjs, l1) && sjs->states[l1] == VALID) {
       ic0 = i0;
-      updated |= tri(sjs, l, l0, l1, ic0);
+      tri(sjs, l, l0, l1, ic0);
     }
   }
 
   for (int i0 = 0, l0; i0 < 8; ++i0) {
     l0 = l + sjs->nb_dl[i0];
     if (inbounds(sjs, l0) && sjs->states[l0] == VALID) {
-      updated |= line(sjs, l, l0);
+      line(sjs, l, l0);
     }
   }
-
-  return updated;
 }
 
 static void adjust(sjs_s *sjs, int l0) {
@@ -472,29 +566,24 @@ void sjs_init(sjs_s *sjs, ivec2 shape, dvec2 xymin, dbl h) {
   heap_init(sjs->heap, capacity, value, setpos, (void *)sjs);
 
   set_nb_dl(sjs);
+  set_cell_nb_verts_dl(sjs);
   set_vert_dl(sjs);
   set_tri_dlc(sjs);
   set_nb_dlc(sjs);
   set_nearby_dlc(sjs);
 
-  // When we build in debug mode, we want to make sure everything
-  // that's allocated on the heap in this function is
-  // initialized. This removes a lot of line noise from Valgrind's
-  // output.
-#if SJS_DEBUG
   for (int lc = 0; lc < sjs->ncells; ++lc) {
     bicubic_invalidate(&sjs->bicubics[lc]);
   }
-#endif
 
   for (int l = 0; l < sjs->nnodes; ++l) {
     sjs->jets[l].f = INFINITY;
-    // Set comment above about initialization in debug mode.
-#if SJS_DEBUG
     sjs->jets[l].fx = NAN;
     sjs->jets[l].fy = NAN;
     sjs->jets[l].fxy = NAN;
-#endif
+  }
+
+  for (int l = 0; l < sjs->nnodes; ++l) {
     sjs->states[l] = FAR;
   }
 }
@@ -542,15 +631,114 @@ static void check_cell_consistency(sjs_s const *sjs, int l0) {
 void sjs_step(sjs_s *sjs) {
   int l0 = heap_front(sjs->heap);
   assert(sjs->states[l0] == TRIAL);
-
   heap_pop(sjs->heap);
+  sjs->states[l0] = VALID;
 
-  // TODO: this is where we could try rebuilding the adjacent cells,
-  // but I'm still not convinced this should be necessary if we do
-  // things correctly below...
+  // Determine which of the cells surrounding l0 are now valid. It's
+  // enough to check if any of the four nearest cells are valid: it's
+  // impossible for them to have been valid (or built before), since
+  // one of their vertices just became valid.
+  bool valid_cell_nb[NUM_NB_CELLS];
+  for (int ic = 0, lc; ic < NUM_NB_CELLS; ++ic) {
+    lc = l2lc(sjs->shape, l0) + sjs->nb_dlc[ic];
+    valid_cell_nb[ic] = cell_is_valid(sjs, lc);
+  }
+
+  // TODO: don't build this inline?
+  bool nb_incident_on_valid_cell_nb[9] = {
+    valid_cell_nb[0],
+    valid_cell_nb[0] || valid_cell_nb[2],
+    valid_cell_nb[2],
+    valid_cell_nb[0] || valid_cell_nb[1],
+    valid_cell_nb[0] || valid_cell_nb[1] || valid_cell_nb[2] || valid_cell_nb[3],
+    valid_cell_nb[2] || valid_cell_nb[3],
+    valid_cell_nb[1],
+    valid_cell_nb[1] || valid_cell_nb[3],
+    valid_cell_nb[3]
+  };
+
+  // Array used to indicated which nearby cells should be used to
+  // compute average values for Txy at cell vertices.
+  bool use_for_Txy_average[NUM_NEARBY_CELLS];
+  memset(use_for_Txy_average, 0x0, sizeof(bool)*NUM_NEARBY_CELLS);
+
+  // Traverse the nearby cells and figure out which should be used to
+  // compute new Txy values.
+  //
+  // NOTE: `use_for_Txy_average` => the cell is inbounds
+  for (int ic = 0, lc; ic < NUM_NEARBY_CELLS; ++ic) {
+    for (int j = 0, i; j < NUM_CELL_VERTS; ++j) {
+      i = cell_verts_to_cell_nb_verts[ic][j];
+      if (i == NO_INDEX) {
+        continue;
+      }
+      use_for_Txy_average[ic] |= nb_incident_on_valid_cell_nb[i];
+    }
+    lc = l2lc(sjs->shape, l0) + sjs->nearby_dlc[ic];
+    use_for_Txy_average[ic] &= cell_is_valid(sjs, lc);
+  }
+
+  // Compute new Txy values at the vertices of the cells that we
+  // decided to use.
+  dvec4 Txy[NUM_NEARBY_CELLS];
+  for (int ic = 0, lc; ic < NUM_NEARBY_CELLS; ++ic) {
+    if (use_for_Txy_average[ic]) {
+      lc = l2lc(sjs->shape, l0) + sjs->nearby_dlc[ic];
+      // If the cell is one of `l0`'s neighbors, then we have to use
+      // bilinear extrapolation to compute its Txy values. Otherwise,
+      // we can just grab the cell's existing Txy values.
+      Txy[ic] = nearby_cell_is_cell_nb[ic] ?
+        interpolate_Txy_at_verts(sjs, lc) :
+        get_cell_Txy_values(sjs, lc);
+    }
+  }
+
+  // TODO: document this
+  {
+    dbl Txy_sum;
+    int nterms;
+    for (int i = 0, l; i < NUM_CELL_NB_VERTS; ++i) {
+      Txy_sum = 0;
+      nterms = 0;
+      for (int j = 0, jc, lc; j < NUM_NB_CELLS; ++j) {
+        jc = cell_nb_verts_to_nearby_cells[i][j];
+        if (use_for_Txy_average[jc]) {
+          lc = l2lc(sjs->shape, l0) + sjs->nearby_dlc[jc];
+          // We effectively access Txy[jc] in reverse here to
+          // efficiently get the Txy value for cell `jc` at the vertex
+          // indexed by `i`. See cell_notes.pdf for a visual depiction
+          // of why this works.
+          Txy_sum += Txy[jc].data[j];
+          ++nterms;
+        }
+      }
+      if (nterms > 0) {
+        l = l0 + sjs->cell_nb_verts_dl[i];
+        sjs->jets[l].fxy = Txy_sum/nterms;
+      }
+    }
+  }
+
+  // Finally, rebuild the cells! Our criterion for whether we should
+  // rebuild a cell is simply whether we recomputed one of its Txy
+  // values, so we can just check `use_for_Txy_average` here.
+  for (int ic = 0, lc; ic < NUM_NEARBY_CELLS; ++ic) {
+    if (use_for_Txy_average[ic]) {
+      lc = l2lc(sjs->shape, l0) + sjs->nearby_dlc[ic];
+      build_cell(sjs, lc);
+    }
+  }
+
+#if SJS_DEBUG
+  check_cell_consistency(sjs, l0);
+#endif
+
+  /**
+   * The section below corresponds to what's done in a "normal"
+   * Dijkstra-like algorithm for solving the eikonal equation.
+   */
 
   // Set FAR nodes to TRIAL and insert them into the heap.
-  sjs->states[l0] = VALID;
   for (int i = 0, l; i < NUM_NB; ++i) {
     l = l0 + sjs->nb_dl[i];
     if (inbounds(sjs, l) && sjs->states[l] == FAR) {
@@ -559,86 +747,14 @@ void sjs_step(sjs_s *sjs) {
     }
   }
 
-  // Array keeping track of which nodes get updated.
-  bool updated[NUM_NB];
-  memset(updated, 0x0, NUM_NB*sizeof(bool));
-
   // Update neighboring nodes.
   for (int i = 0, l; i < NUM_NB; ++i) {
     l = l0 + sjs->nb_dl[i];
     if (inbounds(sjs, l) && sjs->states[l] == TRIAL) {
-      updated[i] = update(sjs, l);
+      update(sjs, l);
       adjust(sjs, l);
     }
   }
-
-  /**
-   * Find adjacent "dirty" cells ("ready" cells which have at least
-   * one updated vertex).
-   *
-   * NOTE: dirty => can_build_cell
-   */
-  bool dirty[NUM_NEARBY_CELLS];
-  memset(dirty, 0x0, NUM_NEARBY_CELLS*sizeof(bool));
-  for (int i = 0, lc; i < NUM_NEARBY_CELLS; ++i) {
-    lc = l2lc(sjs->shape, l0) + sjs->nearby_dlc[i];
-    if (!can_build_cell(sjs, lc)) {
-      continue;
-    }
-    for (int j = 0, k; j < NUM_CELL_VERTS; ++j) {
-      k = dirty2updated[i][j];
-      dirty[i] |= k != NO_INDEX && updated[k];
-    }
-  }
-
-  // Estimate Txy values at the vertices of each dirty cell.
-  //
-  // TODO: this is overkill---we won't actually use all these
-  // values... but it may be more work to try and find the values we
-  // don't need...
-  dbl Txy[NUM_NEARBY_CELLS][4];
-  for (int i = 0, lc; i < NUM_NEARBY_CELLS; ++i) {
-    if (dirty[i]) {
-      lc = l2lc(sjs->shape, l0) + sjs->nearby_dlc[i];
-      est_Txy_values(sjs, lc, Txy[i]);
-    }
-  }
-
-  /**
-   * Set new Txy values at updated nodes to be the average of the Txy
-   * values estimated at each dirty cell.
-   */
-  dbl Txy_sum;
-  int ndirty;
-  for (int i = 0, l; i < NUM_NB; ++i) {
-    Txy_sum = 0;
-    ndirty = 0;
-    for (int j = 0, k; j < NUM_CELL_VERTS; ++j) {
-      k = updated2dirty[i][j];
-      if (dirty[k]) {
-        Txy_sum += Txy[k][j];
-        ++ndirty;
-      }
-    }
-    if (ndirty > 0) {
-      l = l0 + sjs->nb_dl[i];
-      sjs->jets[l].fxy = Txy_sum/ndirty;
-    }
-  }
-
-  for (int i = 0, lc; i < NUM_NEARBY_CELLS; ++i) {
-    if (dirty[i]) {
-      lc = l2lc(sjs->shape, l0) + sjs->nearby_dlc[i];
-      build_cell(sjs, lc);
-  /**
-   * Rebuild dirty cells
-   */
-    }
-  }
-
-#if SJS_DEBUG
-  check_cell_consistency(sjs, l0);
-#endif
 }
 
 void sjs_solve(sjs_s *sjs) {
