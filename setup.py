079cc964a274a2ca6d91dc60cec40ce2481ab099
Samuel F. Potter <sfp@hush.ai>
2020-10-28 17:28:38 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 # distutils: define_macros=NPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION
 
+import glob
+
 from setuptools import Extension, setup
 from Cython.Build import cythonize
 
 extensions = [
     Extension(
         'jmm',
-        ['*.pyx'],
+        ['jmm.pyx'],
         libraries=['jmm'],
         library_dirs=['./build']
-    )
+    ),
 ]
 
 setup(
     name='jmm',
-    ext_modules=cythonize(extensions)
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives={
+            'embedsignature': True,
+            'language_level': 3
+        }
+    )
 )
