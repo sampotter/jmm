b4c8bea0c21344f21ab08d1fb6dad1e7a5d593b2
Samuel F. Potter <sfp@umiacs.umd.edu>
2020-11-05 17:08:55 -0500

    simplify helper

--- helper.py
+++ helper.py
@@ -1,10 +1,8 @@
-import colorcet as cc
-import matplotlib.pyplot as plt
+import jmm
 import meshio
 import numpy as np
-import jmm
-
-plt.ion()
+import pyvista as pv
+import pyvistaqt as pvqt
 
 ################################################################################
 # Set up some test mesh stuff
@@ -19,16 +17,3 @@ cells = mesh.cells_dict['tetra'].astype(np.uintp).copy()
 del mesh
 
 mesh = jmm.Mesh3(verts, cells)
-
-################################################################################
-# Set up some test Bernstein polynomial stuff
-
-r = lambda x: np.linalg.norm(x)
-Dr = lambda x: x/r(x)
-
-X = np.eye(3)
-
-f = np.array([r(x) for x in X])
-Df = np.array([Dr(x) for x in X])
-
-bb = jmm.Bb3Tri(f, Df, X)
