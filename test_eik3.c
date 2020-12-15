48e4a9bb5eb25995e1c2b653cbdae515e2521f31
Samuel F. Potter <sfp@umiacs.umd.edu>
2020-12-15 15:52:53 -0500

    add olim26 tests

--- test_eik3.c
+++ test_eik3.c
@@ -4,9 +4,11 @@
 #include <gsl/gsl_rng.h>
 
 #include <math.h>
+#include <string.h>
 
 #include "eik3.h"
 #include "mesh3.h"
+#include "vec.h"
 
 #define NUM_RANDOM_TRIALS 10
 
@@ -18,7 +20,7 @@ BeforeEach(eik3) {
 
 AfterEach(eik3) {}
 
-Ensure(eik3, tetra) {
+Ensure(eik3, tetra_works_for_olim6_update) {
   gsl_rng *rng = gsl_rng_alloc(gsl_rng_mt19937);
 
   dbl verts[12] = {
@@ -118,6 +120,132 @@ Ensure(eik3, tetra) {
   }
 
   mesh3_deinit(mesh);
+  mesh3_dealloc(&mesh);
+
+  gsl_rng_free(rng);
+}
+
+void get_gt_jet(dbl const xsrc[3], dbl const x[3], jet3 *jet) {
+  dbl tmp[3];
+  dbl3_sub(x, xsrc, tmp);
+  dbl L = dbl3_norm(tmp);
+  jet->f = L;
+  dbl3_dbl_div(tmp, L, &jet->fx);
+}
+
+Ensure(eik3, tetra_works_for_olim26_updates) {
+  gsl_rng *rng = gsl_rng_alloc(gsl_rng_mt19937);
+
+  dbl xsrc[3] = {0, 0, 0};
+
+  dbl verts[12] = {
+    1, 1, 1, // x0
+    2, 1, 1, // x1
+    2, 2, 1, // x2
+    2, 2, 2  // xhat
+  };
+
+  jet3 newjet, jet[3];
+
+  int perm[6][3] = {
+    {0, 1, 2},
+    {0, 2, 1},
+    {1, 0, 2},
+    {2, 0, 1},
+    {1, 2, 0},
+    {2, 1, 0}
+  };
+
+  dbl verts_perm[12];
+  size_t cells_perm[4];
+
+  // Last vertex stays fixed under permutation
+  cells_perm[3] = 3;
+  memcpy((void *)&verts_perm[9], (void *)&verts[9], sizeof(dbl)*3);
+
+  dbl lam[2];
+
+  dbl lam_gt[6][2] = {
+    {0, 0},
+    {0, 0},
+    {1, 0},
+    {1, 0},
+    {0, 1},
+    {0, 1}
+  };
+
+  mesh3_s *mesh;
+  mesh3_alloc(&mesh);
+
+  costfunc_s cf;
+
+  int p;
+  for (int i = 0; i < 6; ++i) {
+    /**
+     * Get vertices for this permutation
+     */
+    for (int j = 0; j < 3; ++j) {
+      cells_perm[j] = p = perm[i][j];
+      memcpy((void *)&verts_perm[3*j], (void *)&verts[3*p], sizeof(dbl)*3);
+    }
+
+    /**
+     * Create a mesh consisting of a single tetrahedron for this update
+     */
+    mesh3_init(mesh, verts_perm, 4, cells_perm, 1);
+
+    /**
+     * Get jets for vertex data
+     */
+    for (int j = 0; j < 3; ++j) {
+      get_gt_jet(xsrc, &verts_perm[3*j],  &jet[j]);
+    }
+
+    costfunc_init(&cf, mesh, jet, 3, 0, 1, 2);
+
+    /**
+     * Verify that cost function has correct nodal values
+     */
+    for (int j = 0; j < 3; ++j) {
+      costfunc_set_lambda(&cf, lam_gt[2*j]);
+      dbl L = dbl3_dist(&verts_perm[3*j], &verts[9]);
+      dbl f = jet[j].f + L;
+      assert_that_double(cf.f, is_equal_to_double(f));
+    }
+
+    /**
+     * Do tetrahedron updates starting from random initial iterates
+     */
+    for (int l = 0; l < NUM_RANDOM_TRIALS; ++l) {
+      /**
+       * Sample random a lambda
+       */
+      lam[0] = gsl_ran_flat(rng, 0, 1);
+      lam[1] = gsl_ran_flat(rng, 0, 1);
+      if (lam[0] + lam[1] > 1) {
+        lam[0] = 1 - lam[0];
+        lam[1] = 1 - lam[1];
+      }
+
+      costfunc_set_lambda(&cf, lam);
+      tetra(&cf, lam, &newjet);
+
+      if (fabs(lam[0]) < 1e-15) {
+        assert_that(fabs(lam_gt[i][0]) < 1e-15);
+      } else {
+        assert_that_double(lam[0], is_equal_to_double(lam_gt[i][0]));
+      }
+
+      if (fabs(lam[1]) < 1e-15) {
+        assert_that(fabs(lam_gt[i][1]) < 1e-15);
+      } else {
+        assert_that_double(lam[1], is_equal_to_double(lam_gt[i][1]));
+      }
+    }
+
+    mesh3_deinit(mesh);
+  }
+
   mesh3_dealloc(&mesh);
 
   gsl_rng_free(rng);
