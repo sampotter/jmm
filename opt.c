23a3efa62ba7de2a74f707b25cf81f362582f051
Samuel F. Potter <sfp@umiacs.umd.edu>
2020-12-20 10:17:16 -0500

    get tripq2 working

--- opt.c
+++ opt.c
@@ -1,8 +1,12 @@
 #include "opt.h"
 
+#include <assert.h>
+
 #include "mat.h"
 
 void triqp2_solve(triqp2_s *qp) {
+  dbl x[2], lam[2]; // Temporary variables used below
+
   /**
    * First, try to compute global minimizer. If it satifies the
    * constraints, we're done.
@@ -19,64 +23,95 @@ void triqp2_solve(triqp2_s *qp) {
   /**
    * Try to minimize over x[0].
    */
-  qp->x[0] = -qp->A[0][0]/qp->b[0];
-  if (0 <= qp->x[0] && qp->x[0] <= 1) {
-    // Check Lagrange multiplier
-    if (qp->b[1] >= 0) {
-      qp->x[1] = 0;
-      return;
+  if (qp->x[1] <= 0) {
+    x[0] = -qp->b[0]/qp->A[0][0];
+    if (0 <= x[0] && x[0] <= 1) {
+      // Check Lagrange multiplier
+      if (qp->A[1][0]*x[0] + qp->b[1] >= 0) {
+        qp->x[0] = x[0];
+        qp->x[1] = 0;
+        return;
+      }
     }
   }
 
   /**
    * Try to minimize over x[1].
    */
-  qp->x[1] = -qp->A[1][1]/qp->b[1];
-  if (0 <= qp->x[1] && qp->x[1] <= 1) {
-    // Check Lagrange multiplier
-    if (qp->b[0] >= 0) {
-      qp->x[0] = 0;
-      return;
+  if (qp->x[0] <= 0) {
+    x[1] = -qp->b[1]/qp->A[1][1];
+    if (0 <= x[1] && x[1] <= 1) {
+      // Check Lagrange multiplier
+      if (qp->A[0][1]*x[1] + qp->b[0] >= 0) {
+        qp->x[0] = 0;
+        qp->x[1] = x[1];
+        return;
+      }
     }
   }
 
   /**
    * Try to minimize over 1 - x[0] - x[1].
    */
-  qp->x[1] = (qp->A[0][0] - qp->A[1][0] + qp->b[0] - qp->b[1])/2;
-  if (0 <= qp->x[1] && qp->x[1] <= 1) {
-    // Check Lagrange multiplier
-    qp->x[0] = 1 - qp->x[1];
-    if (dbl2_dot(qp->A[0], qp->x) + dbl2_dot(qp->A[1], qp->x)
-        + dbl2_sum(qp->b) <= 0) {
-      return;
+  if (dbl2_sum(qp->x) >= 1) {
+    dbl s = qp->A[0][0] - qp->A[1][0] + qp->b[0] - qp->b[1];
+    s /= qp->A[0][0] - qp->A[1][0] - qp->A[0][1] + qp->A[1][1];
+    if (0 <= s && s <= 1) {
+      x[0] = 1 - s;
+      x[1] = s;
+      // Check Lagrange multiplier
+      if (dbl2_dot(qp->A[0], x) + dbl2_dot(qp->A[1], x)
+          + dbl2_sum(qp->b) <= 0) {
+        qp->x[0] = x[0];
+        qp->x[1] = x[1];
+        return;
+      }
     }
   }
 
   /**
    * Check if (0, 0) is optimal.
    */
-  qp->x[0] = qp->x[1] = 0;
-  if (qp->b[0] + qp->b[1] >= 0) {
+  if (qp->b[0] >= 0 && qp->b[1] >= 0) {
+    qp->x[0] = qp->x[1] = 0;
     return;
   }
+
+  // TODO: we should be able to simplify these both significantly and
+  // remove the lambda variable entirely... but let's do that later
 
   /**
    * Check if (1, 0) is optimal.
    */
-  qp->x[0] = 1;
-  dbl lam0 = qp->A[1][0] + qp->b[1];
-  if (lam0 <= 0 && lam0 >= qp->A[0][0] + qp->b[0]) {
-    return;
+  {
+    lam[0] = qp->A[0][0] + qp->b[0];
+    if (lam[0] <= 0) {
+      lam[1] = lam[0] - qp->A[1][0] - qp->b[1];
+      if (lam[1] <= 0) {
+        qp->x[0] = 1;
+        qp->x[1] = 0;
+        return;
+      }
+    }
   }
 
   /**
    * Check if (0, 1) is optimal.
    */
-  qp->x[0] = 0;
-  qp->x[1] = 1;
-  lam0 = qp->A[0][0] + qp->b[0];
-  if (lam0 <= 0 && lam0 >= qp->A[1][0] + qp->b[1]) {
-    return;
+  {
+    lam[0] = qp->A[1][1] + qp->b[1];
+    if (lam[0] <= 0) {
+      lam[1] = lam[0] - qp->A[0][1] - qp->b[0];
+      if (lam[1] <= 0) {
+        qp->x[0] = 0;
+        qp->x[1] = 1;
+        return;
+      }
+    }
   }
+
+  // We shouldn't reach this point! We want to different conditionals
+  // above to exhaust every case and positively identify an argmin for
+  // the quadratic.
+  assert(false);
 }
