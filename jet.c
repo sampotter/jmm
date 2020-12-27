deac4ae806ed68846729119ab4a12b715ac21a1f
Samuel F. Potter <sfp@umiacs.umd.edu>
2020-12-27 11:54:19 -0500

    add jet3_eq

--- jet.c
+++ jet.c
@@ -1,6 +1,11 @@
 #include "jet.h"
 
 #include <math.h>
+#include <string.h>
+
+bool jet3_eq(jet3 const *jet1, jet3 const *jet2) {
+  return !memcmp((void *)jet1, (void *)jet2, sizeof(jet3));
+}
 
 bool jet3_is_finite(jet3 const *jet) {
   return isfinite(jet->f)
--- jet.h
+++ jet.h
@@ -15,6 +15,7 @@ typedef struct {
   dbl fx, fy, fz;
 } jet3;
 
+bool jet3_eq(jet3 const *jet1, jet3 const *jet2);
 bool jet3_is_finite(jet3 const *jet);
 
 #ifdef __cplusplus
