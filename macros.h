ca6712c1e7d76c8504864f166113c493e9204c1f
Samuel F. Potter <sfp@umiacs.umd.edu>
2020-12-28 22:52:11 -0500

    add MIN and MAX macros

--- macros.h
+++ macros.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#define MIN(x, y) ({                            \
+    __typeof(x) x_ = (x);                       \
+    __typeof(y) y_ = (y);                       \
+    x_ < y_ ? x_ : y_;                          \
+  })
+
+#define MAX(x, y) ({                            \
+    __typeof(x) x_ = (x);                       \
+    __typeof(y) y_ = (y);                       \
+    x_ > y_ ? x_ : y_;                          \
+  })
+
 #define SWAP(x, y) {                            \
     __typeof(x) tmp = x;                        \
     x = y;                                      \
