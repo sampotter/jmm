4c555d264fc169e3a58d3c2c600c3bb16978196c
Samuel F. Potter <sfp@umiacs.umd.edu>
2020-12-29 13:35:32 -0500

    add dbl22_isfinite

--- mat.h
+++ mat.h
@@ -12,6 +12,7 @@ void dbl22_add(dbl A[2][2], dbl B[2][2], dbl C[2][2]);
 void dbl22_dbl2_solve(dbl A[2][2], dbl b[2], dbl x[2]);
 dbl dbl22_trace(dbl const A[2][2]);
 void dbl22_dbl2_mul(dbl const A[2][2], dbl const x[2], dbl b[2]);
+bool dbl22_isfinite(dbl const A[2][2]);
 
 void dbl3_outer(dbl u[3], dbl v[3], dbl uv[3][3]);
 void dbl33_add(dbl const A[3][3], dbl const B[3][3], dbl C[3][3]);
