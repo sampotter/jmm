dbe457b191366c6b77a798bfecb76667b5c33e89
Samuel F. Potter <sfp@umiacs.umd.edu>
2020-12-30 18:21:35 -0500

    add comment

--- examples/using_utetra.py
+++ examples/using_utetra.py
@@ -5,6 +5,31 @@ import pyvistaqt as pvqt
 
 
 def F(x, Xt, T, DT):
+    '''Evaluate the solution mapping for a tetrahedron update assuming a
+constant (c = 1 = s) speed of sound.
+
+    - x:    A length 3 array indicating the location of the evaluation point.
+
+    - Xt:   A 3x3 array where each row (i.e., Xt[i] for i = 0, 1, 2) is
+            one of the vertices of the base of the update tetrahedron.
+
+    - T:    A length 3 array where T[i] is the eikonal at Xt[i].
+
+    - DT:   A 3x3 array where row DT[i] is the gradient of the eikonal
+            at point Xt[i].
+
+    Returns a 3-tuple where:
+
+    - The first entry is a length 2 array containing the minimizing
+      argument (lam[0], lam[1]) such that Xt[0] + lam@(Xt[1:] - Xt[0])
+      is the starting point of the optimal ray leading into x.
+
+    - The second entry is the optimal eikonal value.
+
+    - The third entry is a length 3 array with the optimal eikonal
+      gradient.
+
+    '''
     lam0 = np.array([1, 1])/3
     utetra = jmm.UpdateTetra(x, Xt, T, DT)
     utetra.set_lambda(lam0)
@@ -27,7 +52,7 @@ if __name__ == '__main__':
 
     xlam = Xt[0] + lam@(Xt[1:] - Xt[0])
 
-    # Visualize the result...
+    # Visualize the result using PyVista...
     r = 0.04
     plotter = pvqt.BackgroundPlotter()
     for i in range(3):
