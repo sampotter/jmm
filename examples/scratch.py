4045af725548f985c0f8d6b803717ef049d6208b
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-01-04 06:42:07 -0500

    update scratch.py

--- examples/scratch.py
+++ examples/scratch.py
@@ -130,3 +130,143 @@ for i in range(ec.size):
         if L2[j] == l1:
             L1[i + 1], L1[j] = L1[j], L1[i + 1]
             L2[i + 1], L2[j] = L2[j], L2[i + 1]
+
+l = 2812
+vc = set()
+for l0 in mesh.vv(l):
+    for lc in mesh.vc(l0):
+        vc.add(lc)
+vc = np.array(list(vc))
+C = cells[vc]
+plotter = pvqt.BackgroundPlotter()
+cells_ = np.concatenate([4*np.ones((vc.size, 1)), C], axis=1).astype(np.uint64)
+poly_data = pv.PolyData(verts, cells_)
+plotter.add_mesh(poly_data, color='white', opacity=0.3, show_edges=True)
+plotter.add_mesh(pv.Sphere(0.008, verts[l]), color='blue')
+
+def get_vc(l):
+    return np.where((cells == l).any(1))[0]
+
+def get_vv(l):
+    return np.setdiff1d(np.unique(cells[get_vc(l)]), l)
+
+################################################################################
+# Testing out 4-pt updates
+
+from scipy.optimize import LinearConstraint, minimize
+
+l0 = eik.peek()
+l = next(l for l in mesh.vv(l0) if eik.state[l] == jmm.State.Trial.value)
+l1, l2 = [
+    _ for _ in mesh.vv(l)
+    if _ != l0 and eik.state[_] == jmm.State.Valid.value
+][:2]
+
+Ls = []
+for C in cells[mesh.vc(l)]:
+    L = np.array([_ for _ in C if _ != l])
+    if (eik.state[L] == jmm.State.Valid.value).all():
+        Ls.append(L)
+if not Ls:
+    print("didn't find any valid update faces!")
+Ls = np.array(Ls)
+
+def get_opposite_vertex(l, L):
+    return int(np.setdiff1d(cells[
+        (cells != l).all(1) &
+        (cells == L[0]).any(1) &
+        (cells == L[1]).any(1) &
+        (cells == L[2]).any(1)
+    ], L))
+
+xhat = verts[l]
+
+def do_4point_update(L):
+    x = verts[L]
+    dx = x[1:] - x[0]
+
+    f = np.array([eik.jet[_][0] for _ in L])
+    Df = np.array([(eik.jet[_][1], eik.jet[_][2], eik.jet[_][3]) for _ in L])
+
+    bb3tet = jmm.Bb3Tet(f, Df, x)
+
+    def a(i, j):
+        assert i != j
+        tmp = np.zeros((4,), dtype=np.float64)
+        tmp[i] = 1
+        tmp[j] = -1
+        return tmp
+
+    def e(i):
+        return np.eye(4)[i]
+
+    df_dlam = np.array([bb3tet.Df(e(0), a(i, 0)) for i in [1, 2, 3]])
+
+    def xlam(lam):
+        return x[0] + lam@dx
+
+    def L(lam):
+        return np.linalg.norm(xlam(lam) - xhat)
+
+    def dL_dlam(lam):
+        return dx@xlam(lam)/L(lam)
+
+    def F(lam):
+        assert(lam.size == 3)
+        assert(all(lam >= 0))
+        assert(lam.sum() <= 1)
+        b = np.empty((4,))
+        b[0] = 1 - lam.sum()
+        b[1:] = lam
+        return bb3tet.f(b) + L(lam)
+
+    def dF_dlam(lam):
+        assert(lam.size == 3)
+        assert(all(lam >= 0))
+        assert(lam.sum() <= 1)
+        b = np.empty((4,))
+        b[0] = 1 - lam.sum()
+        b[1:] = lam
+        return np.array([bb3tet.Df(e(0), a(i, 0)) for i in [1, 2, 3]]) \
+            + dL_dlam(lam)
+
+    lb = np.array([0, 0, 0, -np.inf])
+    ub = np.array([np.inf, np.inf, np.inf, 1])
+    A = np.array([
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1],
+        [1, 1, 1]
+    ])
+    simplex_constraint = LinearConstraint(A, lb, ub, keep_feasible=True)
+
+    lam0 = np.array([1, 1, 1])/4
+
+    res = minimize(F, lam0, method='trust-constr', jac=dF_dlam,
+                   constraints=[simplex_constraint], tol=1e-12)
+
+    bopt = np.empty((4,))
+    bopt[1:] = res.x
+    bopt[0] = 1 - res.x.sum()
+    print('optimal b: (%1.4f, %1.4f, %1.4f, %1.4f)' % tuple(bopt))
+
+    Topt = res.fun
+    print('optimal T: %1.4f' % Topt)
+
+    DTopt = xhat - bopt@x
+    DTopt /= np.linalg.norm(DTopt)
+    print('optimal DT: (%1.4f, %1.4f, %1.4f)' % tuple(DTopt))
+
+    Terror = Topt - np.linalg.norm(xhat)
+    print('error in T: %1.4f' % Terror)
+
+    DTerror = DTopt - xhat/np.linalg.norm(xhat)
+    print('error in DT: (%1.4f, %1.4f, %1.4f)' % tuple(DTerror))
+
+print('correct T: %1.4f' % np.linalg.norm(xhat))
+print('correct DT: (%1.4f, %1.4f, %1.4f)' % tuple(xhat/np.linalg.norm(xhat)))
+for L in Ls:
+    l3 = get_opposite_vertex(l, L)
+    L = np.concatenate([L, [l3]]).astype(int)
+    print('L = [%d, %d, %d, %d]' % tuple(L))
+    do_4point_update(L)
