950e1642f8a88f4b506395edbe63b732235d9222
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-01-04 06:41:42 -0500

    wrap bb3tet

--- jmm.pyx
+++ jmm.pyx
@@ -47,6 +47,12 @@ cdef extern from "jet.h":
         dbl fy
         dbl fz
 
+cdef extern from "bb.h":
+    void bb3tet_interp3(const dbl f[4], const dbl Df[4][3], const dbl x[4][3], dbl c[20])
+    dbl bb3tet(const dbl c[20], const dbl b[4])
+    dbl dbb3tet(const dbl c[20], const dbl b[4], const dbl a[4])
+    dbl d2bb3tet(const dbl c[20], const dbl b[4], const dbl a[2][4])
+
 cdef extern from "dial.h":
     cdef struct dial3:
         pass
@@ -126,6 +132,58 @@ cdef extern from "utetra.h":
     void utetra_get_jet(const utetra *cf, jet3 *jet)
     void utetra_get_lag_mults(const utetra *cf, dbl alpha[3])
     int utetra_get_num_iter(const utetra *cf)
+
+
+cdef class Bb3Tet:
+    cdef:
+        dbl _c[20]
+
+    def __cinit__(self, dbl[:] f, dbl[:, :] Df, dbl[:, :] x):
+        if f.size != 4 or f.shape[0] != 4:
+            raise Exception('`f` must be a length 4 vector')
+        if Df.size != 12 or Df.shape[0] != 4 or Df.shape[1] != 3:
+            raise Exception('`Df` must have shape (4, 3)')
+        if x.size != 12 or x.shape[0] != 4 or x.shape[1] != 3:
+            raise Exception('`x` must have shape (4, 3)')
+        # cdef int i, j
+        # cdef dbl Df_[4][3]
+        # for i in range(4):
+        #     for j in range(3):
+        #         Df_[i][j] = Df[i, j]
+        # cdef dbl x_[4][3]
+        # for i in range(4):
+        #     for j in range(3):
+        #         x_[i][j] = x[i, j]
+        bb3tet_interp3(
+            &f[0],
+            <const dbl (*)[3]>&Df[0, 0],
+            <const dbl (*)[3]>&x[0, 0],
+            self._c)
+
+    def f(self, dbl[:] b):
+        if b.size != 4 or b.shape[0] != 4:
+            raise Exception('`b` must be a length 4 vector')
+        return bb3tet(self._c, &b[0])
+
+    def Df(self, dbl[:] b, dbl[:] a):
+        if b.size != 4 or b.shape[0] != 4:
+            raise Exception('`b` must be a length 4 vector')
+        if a.size != 4 or a.shape[0] != 4:
+            raise Exception('`a` must be a length 4 vector')
+        return dbb3tet(self._c, &b[0], &a[0])
+
+    def D2f(self, dbl[:] b, dbl[:, :] a):
+        if b.size != 4 or b.shape[0] != 4:
+            raise Exception('`b` must be a length 4 vector')
+        if a.size != 12 or a.shape[0] != 3 or a.shape[4] != 4:
+            raise Exception('`a` must have shape (3, 4)')
+        # cdef int i, j
+        # cdef dbl a_[4][3]
+        # for i in range(4):
+        #     for j in range(3):
+        #         a_[i][j] = a[i, j]
+        return d2bb3tet(self._c, &b[0], <const dbl (*)[4]>&a[0, 0])
+
 
 cdef class UpdateTetra:
     cdef:
