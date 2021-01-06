5345b1a432b9fb9474dc51c48b33127f92998122
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-01-06 10:05:18 -0500

    add mesh3_dbl3_in_cell

--- mesh3.c
+++ mesh3.c
@@ -6,6 +6,7 @@
 #include <string.h>
 
 #include "index.h"
+#include "mat.h"
 #include "util.h"
 
 struct mesh3 {
@@ -244,6 +245,41 @@ void mesh3_get_cell_bbox(mesh3_s const *mesh, size_t i, rect3 *bbox) {
     max[1] = fmax(max[1], mesh->verts[cell[i]].data[1]);
     max[2] = fmax(max[2], mesh->verts[cell[i]].data[2]);
   }
+}
+
+bool mesh3_dbl3_in_cell(mesh3_s const *mesh, size_t lc, dbl const x[3],
+                        dbl b[4]) {
+  assert(lc < mesh->ncells);
+
+  size_t const *c = &mesh->cells[lc].data[0];
+
+  dbl lhs[4][4];
+  for (int j = 0; j < 4; ++j) {
+    lhs[0][j] = 1;
+    for (int i = 1; i < 4; ++i) {
+      lhs[i][j] = mesh->verts[c[j]].data[i - 1];
+    }
+  }
+
+  dbl vol = dbl44_det(lhs);
+
+  dbl rhs[4];
+  rhs[0] = 1;
+  for (int i = 1; i < 4; ++i) {
+    rhs[i] = x[i - 1];
+  }
+
+  dbl tmp[4];
+  for (int j = 0; j < 4; ++j) {
+    dbl44_get_col(lhs, j, tmp);
+    dbl44_set_col(lhs, j, rhs);
+    b[j] = dbl44_det(lhs)/vol;
+    dbl44_set_col(lhs, j, tmp);
+  }
+
+  // After solving this system, the components of b will sum to unity,
+  // so we just need to check if b is nonnegative.
+  return dbl4_nonneg(b);
 }
 
 int mesh3_nvc(mesh3_s const *mesh, size_t i) {
