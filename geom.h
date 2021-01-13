6566be5f08f17907ef81c6904ec530c44c82fdc9
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-01-13 09:18:46 -0500

    add comment

--- geom.h
+++ geom.h
@@ -10,6 +10,13 @@ typedef struct {
   dbl min[3], max[3];
 } rect3;
 
+/**
+ * Compute an approximate convex combination of three unit vectors. The rows
+ * Are
+ *
+ * http://mathforum.org/library/drmath/view/65316.html
+ * https://en.wikipedia.org/wiki/Slerp
+ */
 void lin_comb_unit_vec_3(dbl const t_in[3][3], dbl const b[3], dbl t_out[3]);
 
 #ifdef __cplusplus
