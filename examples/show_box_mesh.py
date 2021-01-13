78f3e6b953638a4fc034fee603f49558f0f4dca6
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-01-13 09:19:19 -0500

    fix bug in show_box_mesh.py

--- examples/show_box_mesh.py
+++ examples/show_box_mesh.py
@@ -6,6 +6,8 @@ surf_mesh = pv.read('room.obj')
 
 grid = pv.read(tet_mesh)
 cells = grid.cells.reshape(-1, 5)[:, 1:]
+points = grid.points.copy().astype(np.float64)
+cells = grid.cells.reshape(-1, 5)[:, 1:].copy().astype(np.uint64)
 centroids = grid.points[cells].mean(1)
 
 mask = centroids[:, 1] < 2
