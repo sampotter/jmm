11bd8abe97c6e5cb2847df42c5cf5d84faeb81b2
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-01-14 09:09:38 -0500

    add ray_and_face_are_coplanar

--- geom.c
+++ geom.c
@@ -3,6 +3,7 @@
 #include <math.h>
 
 #include "mat.h"
+#include "mesh3.h"
 
 void lin_comb_unit_vec_3(dbl const t_in[3][3], dbl const b[3], dbl t_out[3]) {
   for (int i = 0; i < 3; ++i) {
@@ -21,4 +22,22 @@ bool points_are_coplanar(dbl const **x) {
   dbl det = dbl33_det(dx);
   return fabs(det) < 1e-15;
 }
+
+bool ray_and_face_are_coplanar(mesh3_s const *mesh,
+                               size_t l0, size_t l1, size_t l2, dbl const *ray) {
+  dbl const *x[3] = {
+    mesh3_get_vert_ptr(mesh, l0),
+    mesh3_get_vert_ptr(mesh, l1),
+    mesh3_get_vert_ptr(mesh, l2)
+  };
+
+  dbl dx[2][3];
+  dbl3_sub(x[1], x[0], dx[0]);
+  dbl3_sub(x[2], x[0], dx[1]);
+
+  dbl n[3];
+  dbl3_cross(dx[0], dx[1], n);
+  dbl3_normalize(n);
+
+  return fabs(dbl3_dot(n, ray)) < 1e-14;
 }
--- geom.h
+++ geom.h
@@ -6,6 +6,8 @@ extern "C" {
 
 #include "def.h"
 
+typedef struct mesh3 mesh3_s;
+
 typedef struct {
   dbl min[3], max[3];
 } rect3;
@@ -23,6 +25,9 @@ void lin_comb_unit_vec_3(dbl const t_in[3][3], dbl const b[3], dbl t_out[3]);
  * Check if four points are coplanar.
  */
 bool points_are_coplanar(dbl const **x);
+
+bool ray_and_face_are_coplanar(mesh3_s const *mesh,
+                               size_t l0, size_t l1, size_t l2, dbl const *ray);
 
 #ifdef __cplusplus
 }
