63d818087a0048ff4bda55f8682cdd977af61a88
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-01-14 10:13:31 -0500

    remove unused lin_comb_unit_vec_3

--- geom.c
+++ geom.c
@@ -5,15 +5,6 @@
 #include "mat.h"
 #include "mesh3.h"
 
-void lin_comb_unit_vec_3(dbl const t_in[3][3], dbl const b[3], dbl t_out[3]) {
-  for (int i = 0; i < 3; ++i) {
-    t_out[i] = 0;
-    for (int j = 0; j < 3; ++j) {
-      t_out[i] += b[i]*t_in[i][j];
-    }
-  }
-  dbl3_normalize(t_out);
-
 bool points_are_coplanar(dbl const **x) {
   dbl dx[3][3];
   dbl3_sub(x[1], x[0], dx[0]);
--- geom.h
+++ geom.h
@@ -13,15 +13,6 @@ typedef struct {
 } rect3;
 
 /**
- * Compute an approximate convex combination of three unit vectors. The rows
- * Are
- *
- * http://mathforum.org/library/drmath/view/65316.html
- * https://en.wikipedia.org/wiki/Slerp
- */
-void lin_comb_unit_vec_3(dbl const t_in[3][3], dbl const b[3], dbl t_out[3]);
-
-/**
  * Check if four points are coplanar.
  */
 bool points_are_coplanar(dbl const **x);
