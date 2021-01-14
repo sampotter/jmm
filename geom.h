1dabed34aba1f8828b6279a5ef152247b53b002b
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-01-14 10:41:49 -0500

    add tri_area and adj_tetra_pair_is_convex

--- geom.h
+++ geom.h
@@ -20,6 +20,21 @@ bool points_are_coplanar(dbl const **x);
 bool ray_and_face_are_coplanar(mesh3_s const *mesh, size_t l0, size_t l1,
                                size_t l2, dbl const *ray);
 
+/**
+ * Compute the area of a triangle in R^3 with vertices given by `x`,
+ * `y`, and `z`.
+ */
+dbl tri_area(dbl const x[3], dbl const y[3], dbl const z[3]);
+
+/**
+ * Check if a pair of tetrahedra in `mesh` is convex. The tetrahedron
+ * are required to share the face indexed by `lf`. The indices `l0`
+ * and `l1` index the points on either side of the separating face
+ * `lf`.
+ */
+bool adj_tetra_pair_is_convex(mesh3_s const *mesh, size_t l0,
+                              size_t const lf[3], size_t l1);
+
 #ifdef __cplusplus
 }
 #endif
