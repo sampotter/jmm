85d01c2926354f93dde3ea11e46d7946cf52cfb1
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-01-29 18:35:08 -0500

    add jet3_approx_eq and jet3_is_nan

--- jet.c
+++ jet.c
@@ -1,10 +1,18 @@
 #include "jet.h"
 
+#include <assert.h>
 #include <math.h>
 #include <string.h>
 
 jet3 jet3_make_point_source(dbl tau) {
   return (jet3) {.f = tau, .fx = NAN, .fy = NAN, .fz = NAN};
+}
+
+bool jet3_approx_eq(jet3 const *jet1, jet3 const *jet2, dbl atol) {
+  return fabs(jet1->f - jet2->f) <= atol &&
+    fabs(jet1->fx - jet2->fx) <= atol &&
+    fabs(jet1->fy - jet2->fy) <= atol &&
+    fabs(jet1->fz - jet2->fz) <= atol;
 }
 
 bool jet3_eq(jet3 const *jet1, jet3 const *jet2) {
@@ -14,4 +22,13 @@ bool jet3_eq(jet3 const *jet1, jet3 const *jet2) {
 bool jet3_is_finite(jet3 const *jet) {
   return isfinite(jet->f)
     && isfinite(jet->fx) && isfinite(jet->fy) && isfinite(jet->fz);
+}
+
+bool jet3_is_nan(jet3 const *jet) {
+  bool is_nan = isnan(jet->fx);
+  if (is_nan) {
+    assert(isnan(jet->fy)); // Consistency check... If one of the
+    assert(isnan(jet->fz)); // partials is NaN, all of them should be.
+  }
+  return is_nan;
 }
--- jet.h
+++ jet.h
@@ -16,8 +16,10 @@ typedef struct {
 } jet3;
 
 jet3 jet3_make_point_source(dbl tau);
+bool jet3_approx_eq(jet3 const *jet1, jet3 const *jet2, dbl atol);
 bool jet3_eq(jet3 const *jet1, jet3 const *jet2);
 bool jet3_is_finite(jet3 const *jet);
+bool jet3_is_nan(jet3 const *jet);
 
 #ifdef __cplusplus
 }
