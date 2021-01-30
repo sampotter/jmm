aea434527adf6f1a8763c3486c26d4ba4ab98ab5
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-01-29 22:23:22 -0500

    remove unused stuff

--- slowness_and_uexact.h
+++
@@ -1,6 +0,0 @@
-double slowness(char slo_fun,struct myvector x);
-struct myvector gradslo(char slo_fun,struct myvector x,double s);
-struct mymatrix Hslo(char slo_fun,struct myvector x,struct myvector gs,double s);
-double exact_solution(char slo_fun,struct myvector x,double s);
-struct myvector exact_gradient(char slo_fun,struct myvector x,double s);
-void set_params( char slo_fun,struct myvector *xstart );
