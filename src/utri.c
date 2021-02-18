fd1c45adaec706c0536201f9d2164d562c537f6e
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-02-18 15:54:56 -0500

    deinit and dealloc cells array in utri_update_ray_is_physical

--- src/utri.c
+++ src/utri.c
@@ -224,6 +224,11 @@ bool utri_update_ray_is_physical(utri_s const *utri, eik3_s const *eik) {
       break;
   }
 
+  // Free the space used for the adjacent cells since we're done with
+  // it now
+  array_deinit(cells);
+  array_dealloc(&cells);
+
   // If we didn't find a containing cell, we can conclude that the ray
   // is unphysical!
   if (!xm_in_cell || !xp_in_cell)
