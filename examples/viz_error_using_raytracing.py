20c98868da2842e565fc63b056092a3fa46cc13a
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-02-19 14:19:33 -0500

    add more to viz_error_using_raytracing.py

--- examples/viz_error_using_raytracing.py
+++ examples/viz_error_using_raytracing.py
@@ -10,6 +10,8 @@ import scipy.optimize
 
 from common import get_camera_basis, get_view_direction
 
+plt.ion()
+
 LEVEL = 0.5
 
 if __name__ == '__main__':
@@ -48,3 +50,51 @@ if __name__ == '__main__':
     level_rtree = base_rtree.copy()
     level_rtree.insert(level_bmesh.mesh)
     level_rtree.build()
+
+    # Compute camera frame (front x left x up)
+    origin = np.array([-4, 0, 0], dtype=np.float64)
+    target = np.array([0, 0, 0], dtype=np.float64)
+    up = np.array([0, 0, 1])
+    left, front, up = get_camera_basis(origin, target, up)
+
+    # Image size
+    w, h = 512, 512
+    num_rays = w*h
+
+    # Camera parameters
+    s = w/h # aspect ratio
+    fov_y = 45 # vertical field of view
+
+    Theta = np.deg2rad(np.linspace(-s*fov_y/2, s*fov_y/2, w))
+    Phi = np.deg2rad(np.linspace(-fov_y/2, fov_y/2, h))
+
+    orgs = np.outer(np.ones(num_rays), origin)
+    dirs = np.array([get_view_direction(left, front, up, phi, theta)
+                     for phi, theta in it.product(Phi, Theta)])
+
+    T = np.empty((num_rays,), dtype=np.float64)
+    I = np.empty((num_rays,), dtype=np.float64)
+    for l, (org, dir_) in enumerate(zip(orgs, dirs)):
+        isect = level_rtree.intersect(org, dir_)
+        T[l] = isect.t if isect.hit else np.nan
+        I[l] = isect.obj.astype(jmm.Mesh2Tri).index if isect.hit else np.nan
+    T = T.reshape(h, w)
+    I = I.reshape(h, w)
+
+    plt.figure(figsize=(9, 4))
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(I, cmap=cc.cm.rainbow, interpolation='none')
+    plt.gca().set_aspect('equal')
+    plt.colorbar()
+    plt.title("index")
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(T, cmap=cc.cm.bmw)
+    plt.gca().set_aspect('equal')
+    plt.colorbar()
+    plt.title("$t$")
+
+    plt.tight_layout()
+
+    plt.show()
