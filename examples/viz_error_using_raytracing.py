4610f2282303f19dfdd34991e9e15c1899bd8b1a
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-03-08 17:38:14 -0800

    nearly done with viz_error_using_raytracing.py! (wip)

--- examples/viz_error_using_raytracing.py
+++ examples/viz_error_using_raytracing.py
@@ -5,6 +5,7 @@ import itertools as it
 import jmm
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import numpy as np
 import os
 import scipy.optimize
@@ -12,10 +13,7 @@ import scipy.optimize
 from common import get_camera_basis, get_view_direction
 from PIL import Image
 
-matplotlib.use('agg')
 plt.ion()
-
-LEVEL = 0.5
 
 if __name__ == '__main__':
     path = '.'
@@ -25,135 +23,125 @@ if __name__ == '__main__':
     verts_bin_path = os.path.join(path, scene + '_verts.bin')
     verts = np.fromfile(verts_bin_path, np.float64)
     verts = verts.reshape(verts.size//3, 3)
+
     print('- read verts from %s' % verts_bin_path)
 
     cells_bin_path = os.path.join(path, scene + '_cells.bin')
     cells = np.fromfile(cells_bin_path, np.uintp)
     cells = cells.reshape(cells.size//4, 4)
+
     print('- reading cells from %s' % cells_bin_path)
 
     mesh = jmm.Mesh3.from_verts_and_cells(verts, cells)
     eik = jmm.Eik3(mesh)
     eik.add_trial(indsrc, jmm.Jet3(0, np.nan, np.nan, np.nan))
     eik.solve()
+
     print('- solved point source problem')
 
+    errors = []
+    for vert, jet in zip(verts, eik.jet):
+        tau = np.linalg.norm(vert)
+        errors.append(jet[0] - tau)
+    errors = np.array(errors)
+
+    # plt.figure()
+    # plt.scatter(np.sqrt(np.sum(verts**2, axis=1)), errors, s=3, c='k')
+    # plt.show()
+
+    e_max = abs(errors).max()
+
+    print('- computed errors (max abs error: %g)' % e_max)
+
     bmesh = jmm.Bmesh33.from_eik3(eik)
+
     print('- assembled Bernstein-Bezier mesh for solution')
 
-    # Initialize an R-tree (don't build the BVH yet!) with the
-    # triangles in the surface mesh of our domain. For each level in
-    # our video, we'll copy this R-tree, insert the tetrahedra for the
-    # current level, and build the R-tree before raytracing and
-    # visualizing.
-    surface_mesh = mesh.get_surface_mesh()
-    base_rtree = jmm.Rtree()
-    base_rtree.insert(surface_mesh)
-    print('- got surface mesh and inserted into BVH')
+    # Set up camera
+    pos = (-2, 0, 0)
+    look = (1, 0, 0)
+    up = (0, 0, 1)
+    left = np.cross(up, look)
+    shape = (512, 512)
+    width = 2.02
+    height = 2.02
+    camera = jmm.Camera.make_orthographic(pos, look, left, up, shape, width, height)
+    print('- set up camera')
 
-    level_bmesh = bmesh.get_level_bmesh(LEVEL)
-    print('- got Bernstein-Bezier mesh for level (T = %f)' % LEVEL)
+    levels = np.linspace(0.0, 1.8, 18*30 + 1)
 
-    level_rtree = base_rtree.copy()
-    level_rtree.insert(level_bmesh.mesh)
-    level_rtree.build()
-    print('- built BVH for level set')
+    for i, LEVEL in enumerate(levels):
+        print('level = %g (%d/%d)' % (LEVEL, i + 1, levels.size))
 
-    # Compute camera frame (front x left x up)
-    origin = np.array([-4, 0, 0], dtype=np.float64)
-    target = np.array([0, 0, 0], dtype=np.float64)
-    up = np.array([0, 0, 1])
-    left, front, up = get_camera_basis(origin, target, up)
+        level_bmesh = bmesh.restrict_to_level(LEVEL)
+        print('- got Bernstein-Bezier mesh for level (T = %f)' % LEVEL)
 
-    # Image size
-    w, h = 512, 512
-    num_rays = w*h
+        level_rtree = jmm.Rtree()
+        level_rtree.insert(level_bmesh)
+        level_rtree.build()
+        print('- built BVH for level set')
 
-    # Camera parameters
-    s = w/h # aspect ratio
-    fov_y = 45 # vertical field of view
+        T = np.empty((camera.num_rays,), dtype=np.float64)
+        I = np.empty((camera.num_rays,), dtype=np.float64)
+        # Type = np.empty((camera.num_rays,), dtype=np.float64)
+        Error = np.empty((camera.num_rays,), dtype=np.float64)
 
-    Theta = np.deg2rad(np.linspace(-s*fov_y/2, s*fov_y/2, w))
-    Phi = np.deg2rad(np.linspace(-fov_y/2, fov_y/2, h))
+        # Set default values
+        T[...] = np.nan
+        I[...] = np.nan
+        # Type[...] = np.nan
+        Error[...] = np.nan
 
-    orgs = np.outer(np.ones(num_rays), origin)
-    dirs = np.array([get_view_direction(left, front, up, phi, theta)
-                     for phi, theta in it.product(Phi, Theta)])
-    print('- set up camera rays')
+        for l, ray in enumerate(camera.get_image_rays()):
+            isect = level_rtree.intersect(ray)
 
-    T = np.empty((num_rays,), dtype=np.float64)
-    I = np.empty((num_rays,), dtype=np.float64)
-    Type = np.empty((num_rays,), dtype=np.float64)
-    for l, (org, dir_) in enumerate(zip(orgs, dirs)):
-        assert abs(1 - np.linalg.norm(dir_)) < 1e-15
-        isect = level_rtree.intersect(org, dir_)
-        T[l] = isect.t if isect.hit else np.nan
-        I[l] = isect.obj.astype(jmm.Mesh2Tri).index if isect.hit else np.nan
-        Type[l] = isect.obj.type.value if isect.hit else np.nan
-    T = T.reshape(h, w)
-    I = I.reshape(h, w)
-    Type = Type.reshape(h, w)
-    print('- finished raytracing')
+            if not isect.hit:
+                continue
 
-    plt.figure(figsize=(12, 4))
+            T[l] = isect.t
+            I[l] = isect.obj.astype(jmm.Bmesh33Cell).index
+            # Type[l] = isect.obj.type.value
+            Error[l] = LEVEL - np.linalg.norm(ray.get_point(T[l]))
 
-    plt.subplot(1, 3, 1)
-    plt.imshow(I, cmap=cc.cm.rainbow, interpolation='none')
-    plt.gca().set_aspect('equal')
-    plt.colorbar()
-    plt.title("index")
 
-    plt.subplot(1, 3, 2)
-    plt.imshow(T, cmap=cc.cm.bmw)
-    plt.gca().set_aspect('equal')
-    plt.colorbar()
-    plt.title("$t$")
+        T = T.reshape(shape)
+        I = I.reshape(shape)
+        # Type = Type.reshape(shape)
+        Error = Error.reshape(shape)
 
-    plt.subplot(1, 3, 3)
-    plt.imshow(Type, cmap=cc.cm.rainbow)
-    plt.gca().set_aspect('equal')
-    plt.colorbar()
-    plt.title('Type')
+        print('- finished raytracing')
 
-    plt.tight_layout()
+        fig = plt.figure(figsize=(11, 6))
 
-    plt.show()
+        gs = gridspec.GridSpec(2, 3, figure=fig)
 
-    print('- finished making plot')
+        ax = fig.add_subplot(gs[0, 0])
+        im = ax.imshow(I, cmap=cc.cm.rainbow, interpolation='none')
+        fig.colorbar(im, ax=ax)
+        ax.set_aspect('equal')
+        ax.set_title("cell index")
 
-    tet_inds = set()
-    TriColor = np.array([0.5, 0.5, 1.0])
-    TriAlpha = 0.1
-    Img = np.empty((h, w, 3), dtype=np.float64)
-    Img[:] = 1
-    for (i, j), org, dir_ in zip(it.product(range(h), range(w)), orgs.copy(), dirs.copy()):
-        isect = level_rtree.intersect(org, dir_)
-        while isect.hit:
-            if isect.obj.type.value == 0: # Mesh2Tri
-                Img[i, j] *= 1 - TriAlpha
-                Img[i, j] += TriAlpha*TriColor
-            elif isect.obj.type.value == 1:
-                tetra = isect.obj.astype(jmm.Mesh3Tetra)
-                l = tetra.index
-                tet_inds.add(l) # For debugging...
-                cell = level_bmesh.get_cell(tetra.index)
-                ray = jmm.Ray3(org, dir_)
-                print('cell.ray_intersects_level(l = %d)' % (l,))
-                b = cell.ray_intersects_level(ray, LEVEL)
-                if b is not None:
-                    print(b)
-            org += (isect.t + 1e-10)*dir_
-            isect = level_rtree.intersect(org, dir_)
-    print('- finished raytracing for image')
-    plt.figure()
-    plt.imshow(Img, cmap=cc.cm.gray, vmin=0, vmax=1, interpolation='none')
-    plt.gca().set_aspect('equal')
-    plt.tight_layout()
-    plt.show()
-    print('- plotted image')
+        ax = fig.add_subplot(gs[1, 0])
+        im = ax.imshow(T, cmap=cc.cm.bmw)
+        fig.colorbar(im, ax=ax)
+        ax.set_aspect('equal')
+        ax.set_title("$t$")
 
-    f = np.array([0, 1, 1, 1], dtype=np.float64)
-    Df = np.zeros((4, 3), dtype=np.float64); Df[1, 0] = 1; Df[2, 1] = 1; Df[3, 2] = 1;
-    X = Df.copy()
-    bb = jmm.Bb33.from_3d_data(f, Df, X)
-    print('- set up test Bezier tetrahedron')
+        # Set the clim based on the 99% quantile of the absolute errors
+        vmax = np.quantile(abs(errors), q=0.99)
+
+        ax = fig.add_subplot(gs[:, 1:])
+        im = ax.imshow(Error, vmin=-vmax, vmax=vmax, cmap=cc.cm.coolwarm)
+        fig.colorbar(im, ax=ax)
+        ax.set_aspect('equal')
+        ax.set_title(r'$e(x) = T(x) - \tau(x)$')
+
+        fig.suptitle('level = %1.4f' % LEVEL)
+
+        fig.tight_layout()
+
+        fig.savefig('frames/frame%03d.png' % i)
+
+        plt.close(fig)
+
+        print('- finished making "info" plot')
