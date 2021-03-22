0dd7966ceec90566c9ba942519e16ff50df1eaec
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-03-21 17:55:57 -0700

    fix setup.sh

--- examples/make_L_video/setup.sh
+++ examples/make_L_video/setup.sh
@@ -8,11 +8,12 @@ cd ../common
 
 ./make_L_shaped_room_mesh.py
 ./strip_off_comments.sh $OFF_PATH
-./tetrahedralize.sh $OFF_PATH $MAX_VOLUME
-./extract_verts_and_cells_from_tet_mesh.py --root=L
+./tetrahedralize.sh $OFF_PATH $MAX_VOLUME > tetgen_output.txt
+./extract_verts_and_cells_from_tet_mesh.py --path=L.1.vtk
 
 rm L.1.smesh L.1.vtk L.obj L.off
 
-cd -
+cd ~-
 
+mv ../common/tetgen_output.txt .
 mv ../common/L_{verts,cells}.bin .
