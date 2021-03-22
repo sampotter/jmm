43347989323bb8c6f968d28448d6bb55452716b9
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-03-21 17:53:35 -0700

    fix extract_verts_and_cells_from_tet_mesh.py

--- examples/common/extract_verts_and_cells_from_tet_mesh.py
+++ examples/common/extract_verts_and_cells_from_tet_mesh.py
@@ -6,35 +6,23 @@ import numpy as np
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--root', type=str)
-    parser.add_argument('--xsrc', type=str, default="(0, 0, 0)")
+    parser.add_argument('--path', type=str)
+
     args = parser.parse_args()
 
-    root = args.root
+    mesh = meshio.read(args.path)
+    print('- read %s' % args.path)
 
-    mesh_path = '%s.1.vtk' % root
-    mesh = meshio.read(mesh_path)
-    print('- read %s' % mesh_path)
+    scene_name = args.path.split('.')[0]
+    print('- using scene name "%s"' % scene_name)
 
-    xsrc = eval(args.xsrc)
-    print('- using %s for point source location' % (xsrc,))
-    xsrc = np.array(xsrc, dtype=np.float64)
-
-    verts_bin_path = '%s_verts.bin' % root
+    verts_bin_path = '%s_verts.bin' % scene_name
     verts = mesh.points.astype(np.float64)
-    indsrc = np.argmin(np.sqrt(np.sum((xsrc - verts)**2, axis=1)))
-    if np.linalg.norm(xsrc - verts[indsrc]) > np.finfo(np.float64).resolution:
-        print('- rounding point closest to origin to point source')
-        verts[indsrc, :] = xsrc
-        mesh.points[indsrc, :] = xsrc
-        mesh.write(mesh_path)
-        print('- wrote corrected %s' % mesh_path)
-    print('- point source index: %d' % indsrc)
     with open(verts_bin_path, 'wb') as f:
         verts.tofile(f)
     print('- wrote %s' % verts_bin_path)
 
-    cells_bin_path = '%s_cells.bin' % root
+    cells_bin_path = '%s_cells.bin' % scene_name
     cells = mesh.cells_dict['tetra'].astype(np.uintp)
     with open(cells_bin_path, 'wb') as f:
         cells.tofile(f)
