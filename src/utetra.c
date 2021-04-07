d97049d329df1cd50e7686eb72d83b24b5d5f227
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-04-07 17:08:41 -0400

    add some more utetra utility functions

--- src/utetra.c
+++ src/utetra.c
@@ -90,6 +90,16 @@ bool utetra_init_from_ptrs(utetra_s *cf, mesh3_s const *mesh, jet3 const *jet,
   return utetra_init(cf, x, Xt, jet_);
 }
 
+bool utetra_init_no_inds(utetra_s *cf, dbl const x[3], dbl const Xt[3][3],
+                         jet3 const jet[3]) {
+  // Set the indices to `NO_INDEX` here to ensure that we don't
+  // accidentally trip over weird things later that assume access to
+  // indices that make sense.
+  cf->lhat = cf->l[0] = cf->l[1] = cf->l[2] = NO_INDEX;
+
+  return utetra_init(cf, x, Xt, jet);
+}
+
 bool utetra_init(utetra_s *cf, dbl const x[3], dbl const Xt[3][3],
                  jet3 const jet[3]) {
   // Initialize lambda with a dummy value so that we can check for bad
@@ -424,7 +434,20 @@ int utetra_get_interior_coefs_mask(utetra_s const *utetra, bool I[3]) {
   return I[0] + I[1] + I[2];
 }
 
+bool utetra_update_inds_are_set(utetra_s const *utetra) {
+  return !(
+    utetra->l[0] == (size_t)NO_INDEX ||
+    utetra->l[1] == (size_t)NO_INDEX ||
+    utetra->l[2] == (size_t)NO_INDEX);
+}
+
+bool utetra_inds_are_set(utetra_s const *utetra) {
+  return utetra->lhat != (size_t)NO_INDEX && utetra_update_inds_are_set(utetra);
+}
+
 bool utetra_update_ray_is_physical(utetra_s const *utetra, eik3_s const *eik) {
+  assert(utetra_inds_are_set(utetra));
+
   size_t const *l = utetra->l;
 
   mesh3_s const *mesh = eik3_get_mesh(eik);
@@ -596,10 +619,13 @@ int utetra_get_num_interior_coefs(utetra_s const *utetra) {
 }
 
 void utetra_get_update_inds(utetra_s const *utetra, size_t l[3]) {
+  assert(utetra_update_inds_are_set(utetra));
   memcpy(l, utetra->l, sizeof(size_t[3]));
 }
 
 bool utetra_has_shadow_solution(utetra_s const *utetra, eik3_s const *eik) {
+  assert(utetra_inds_are_set(utetra));
+
   dbl const atol = 1e-14;
 
   dbl b[3];
@@ -616,6 +642,9 @@ bool utetra_has_shadow_solution(utetra_s const *utetra, eik3_s const *eik) {
 }
 
 int utetra_get_num_shared_inds(utetra_s const *u1, utetra_s const *u2) {
+  assert(utetra_inds_are_set(u1));
+  assert(utetra_inds_are_set(u2));
+
   size_t const *l1 = u1->l, *l2 = u2->l;
   assert(l1[0] != l1[1] && l1[1] != l1[2]);
   assert(l2[0] != l2[1] && l2[1] != l2[2]);
@@ -664,13 +693,33 @@ bool utetras_yield_same_update(utetra_s const **utetra, int n) {
 }
 
 size_t utetra_get_l(utetra_s const *utetra) {
+  assert(utetra_inds_are_set(utetra));
   return utetra->lhat;
+}
+
+void utetra_set_update_inds(utetra_s *utetra, size_t l[3]) {
+  memcpy(utetra->l, l, sizeof(size_t[3]));
+}
+
+void utetra_set_l0(utetra_s *utetra, size_t l0) {
+  utetra->l[0] = l0;
+}
+
+void utetra_set_l1(utetra_s *utetra, size_t l1) {
+  utetra->l[1] = l1;
+}
+
+void utetra_set_l2(utetra_s *utetra, size_t l2) {
+  utetra->l[2] = l2;
 }
 
 /**
  * Check whether the optimum of `u1` is incident on the base of `u2`.
  */
 bool utetra_opt_inc_on_other_utetra(utetra_s const *u1, utetra_s const *u2) {
+  assert(utetra_inds_are_set(u1));
+  assert(utetra_inds_are_set(u2));
+
   dbl b1[3];
   utetra_get_bary_coords(u1, b1);
 
@@ -722,6 +771,8 @@ void utetra_get_x(utetra_s const *u, dbl x[3]) {
 }
 
 void utetra_get_interior_coefs(utetra_s const *utetra, size_t *l) {
+  assert(utetra_update_inds_are_set(utetra));
+
   dbl const atol = 1e-14;
 
   size_t i = 0;
@@ -734,4 +785,29 @@ void utetra_get_interior_coefs(utetra_s const *utetra, size_t *l) {
 
   if (utetra->lam[0] + utetra->lam[1] < 1 - atol)
     l[i++] = utetra->l[0];
+}
+
+size_t utetra_get_active_inds(utetra_s const *utetra, size_t l[3]) {
+  assert(utetra_update_inds_are_set(utetra));
+
+  dbl const atol = 1e-14;
+
+  dbl alpha[3];
+  utetra_get_lag_mults(utetra, alpha);
+
+  size_t num_active = 0;
+
+  l[0] = l[1] = l[2] = NO_INDEX;
+  for (int i = 0; i < 3; ++i)
+    if (fabs(alpha[i]) < atol)
+      l[num_active++] = utetra->l[i];
+
+  return num_active;
+}
+
+par3_s utetra_get_parent(utetra_s const *utetra) {
+  par3_s par;
+  utetra_get_bary_coords(utetra, par.b);
+  utetra_get_update_inds(utetra, par.l);
+  return par;
 }
--- src/utetra.h
+++ src/utetra.h
@@ -7,6 +7,7 @@ extern "C" {
 #include "common.h"
 #include "geom.h"
 #include "jet.h"
+#include "par.h"
 
 typedef struct utetra utetra_s;
 
@@ -17,6 +18,8 @@ bool utetra_init_from_eik3(utetra_s *cf, eik3_s const *eik,
                            size_t l, size_t l0, size_t l1, size_t l2);
 bool utetra_init_from_ptrs(utetra_s *cf, mesh3_s const *mesh, jet3 const *jet,
                            size_t l, size_t l0, size_t l1, size_t l2);
+bool utetra_init_no_inds(utetra_s *cf, dbl const x[3], dbl const Xt[3][3],
+                         jet3 const jet[3]);
 bool utetra_init(utetra_s *cf, dbl const x[3], dbl const Xt[3][3],
                  jet3 const jet[3]);
 bool utetra_is_degenerate(utetra_s const *cf);
@@ -38,6 +41,7 @@ bool utetra_diff(utetra_s const *utetra, mesh3_s const *mesh, size_t const l[3])
 ray3 utetra_get_ray(utetra_s const *utetra);
 void utetra_get_point_on_ray(utetra_s const *utetra, dbl t, dbl xt[3]);
 int utetra_get_interior_coefs_mask(utetra_s const *utetra, bool I[3]);
+bool utetra_inds_are_set(utetra_s const *utetra);
 bool utetra_update_ray_is_physical(utetra_s const *utetra, eik3_s const *eik);
 int utetra_get_num_interior_coefs(utetra_s const *utetra);
 void utetra_get_update_inds(utetra_s const *utetra, size_t l[3]);
@@ -45,9 +49,15 @@ bool utetra_has_shadow_solution(utetra_s const *utetra, eik3_s const *eik);
 int utetra_get_num_shared_inds(utetra_s const *u1, utetra_s const *u2);
 bool utetras_yield_same_update(utetra_s const **utetra, int n);
 size_t utetra_get_l(utetra_s const *utetra);
+void utetra_set_update_inds(utetra_s *utetra, size_t l[3]);
+void utetra_set_l0(utetra_s *utetra, size_t l0);
+void utetra_set_l1(utetra_s *utetra, size_t l1);
+void utetra_set_l2(utetra_s *utetra, size_t l2);
 bool utetra_opt_inc_on_other_utetra(utetra_s const *u1, utetra_s const *u2);
 void utetra_get_x(utetra_s const *u, dbl x[3]);
 void utetra_get_interior_coefs(utetra_s const *utetra, size_t *l);
+size_t utetra_get_active_inds(utetra_s const *utetra, size_t l[2]);
+par3_s utetra_get_parent(utetra_s const *utetra);
 
 #ifdef __cplusplus
 }
