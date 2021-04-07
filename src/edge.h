c1edde68c1f887ed73d65d65ad87b008f5416174
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-04-07 17:08:08 -0400

    add some functions to edge.h

--- src/edge.h
+++ src/edge.h
@@ -4,14 +4,20 @@
 extern "C" {
 #endif
 
-#include <stdbool.h>
-#include <stddef.h>
+#include "def.h"
+
+typedef struct eik3 eik3_s;
+typedef struct mesh3 mesh3_s;
 
 typedef struct edge {
   size_t l[2];
 } edge_s;
 
 edge_s make_edge(size_t l0, size_t l1);
+size_t edge_get_valid_index(edge_s edge, eik3_s const *eik);
+size_t edge_get_shadow_index(edge_s edge, eik3_s const *eik);
+void edge_get_x0_and_dx(edge_s edge, mesh3_s const *mesh, dbl x0[3], dbl dx[3]);
+void edge_get_xt(edge_s edge, mesh3_s const *mesh, dbl t, dbl xt[3]);
 
 #ifdef __cplusplus
 }
