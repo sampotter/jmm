3287400b324527d66b0396f250f7264b32ac5bcb
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-04-27 14:25:22 -0400

    update sethian_shadow.py

--- examples/sethian_shadow/sethian_shadow.py
+++ examples/sethian_shadow/sethian_shadow.py
@@ -6,13 +6,26 @@ import numpy as np
 import pyvista as pv
 import pyvistaqt as pvqt
 import sys
+import time
 import vtk
+
+t0 = np.inf
+
+def tic():
+    global t0
+    t0 = time.time()
+
+def toc():
+    global t0
+    return time.time() - t0
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('scene', type=str)
     parser.add_argument('lsrc', type=int)
     args = parser.parse_args()
+
+    tic()
 
     try:
         surf_grid = pv.read(f'{args.scene}.obj')
@@ -42,15 +55,23 @@ if __name__ == '__main__':
     dom_cells = cells[:num_dom_cells]
     dom_points = points[:num_dom_points]
 
+    print(f'- set up meshes [%1.2fs]' % toc())
+
+    tic()
+
     mesh = jmm.Mesh3.from_verts_and_cells(points, cells)
     eik = jmm.Eik3(mesh)
     eik.add_trial(args.lsrc, jmm.Jet3(0, np.nan, np.nan, np.nan))
     eik.solve()
 
+    print('- solved on extended domain [%1.2fs]' % toc())
+
     mesh_dom = jmm.Mesh3.from_verts_and_cells(dom_points, dom_cells)
     eik_dom = jmm.Eik3(mesh_dom)
     eik_dom.add_trial(args.lsrc, jmm.Jet3(0, np.nan, np.nan, np.nan))
     eik_dom.solve()
+
+    print('- solved on domain [%1.2fs]' % toc())
 
     T = np.array([_[0] for _ in eik.jet[:num_dom_points]])
     T_diff = np.array([_[0] for _ in eik_dom.jet])
@@ -59,19 +80,23 @@ if __name__ == '__main__':
     T_diff[~np.isfinite(T_diff)] = -abs(T_diff[np.isfinite(T_diff)]).max()
 
     D = T_diff - T
-    D[D != 0] /= np.maximum(T_diff[D != 0], T[D != 0])
-
-    Z = (D < 0.1).astype(np.float64)
+    Z = D > 0.007
 
     dom_grid = pv.UnstructuredGrid({vtk.VTK_TETRA: dom_cells}, dom_points)
     dom_grid.point_arrays['Z'] = Z
     dom_grid.point_arrays['T'] = T
     dom_grid.point_arrays['T_diff'] = T_diff
 
-    clipped_dom_grid = dom_grid.clip('y')
-
-    plotter = pvqt.BackgroundPlotter()
-    # plotter.add_mesh(clipped_dom_grid, scalars='Z', cmap=cc.cm.gray_r)
-    # plotter.add_mesh(clipped_dom_grid, scalars='T', cmap=cc.cm.coolwarm)
-    plotter.add_mesh(clipped_dom_grid, scalars='T_diff', cmap=cc.cm.coolwarm)
-    plotter.add_mesh(pv.Sphere(0.1, points[args.lsrc]), color='pink')
+    if points[args.lsrc, 1] in {0, 4.5}:
+        plotter = pvqt.BackgroundPlotter()
+        plotter.add_mesh(dom_grid, scalars='Z', cmap=cc.cm.gray_r, show_scalar_bar=False)
+        # plotter.add_mesh(dom_grid, scalars='T', cmap=cc.cm.coolwarm)
+        # plotter.add_mesh(dom_grid, scalars='T_diff', cmap=cc.cm.coolwarm)
+        plotter.add_mesh(pv.Sphere(0.1, points[args.lsrc]), color='pink')
+    else:
+        clipped_dom_grid = dom_grid.clip('y', origin=(0, points[args.lsrc, 1], 0))
+        plotter = pvqt.BackgroundPlotter()
+        plotter.add_mesh(clipped_dom_grid, scalars='Z', cmap=cc.cm.gray_r, show_scalar_bar=False)
+        # plotter.add_mesh(clipped_dom_grid, scalars='T', cmap=cc.cm.coolwarm)
+        # plotter.add_mesh(clipped_dom_grid, scalars='T_diff', cmap=cc.cm.coolwarm)
+        plotter.add_mesh(pv.Sphere(0.1, points[args.lsrc]), color='pink')
