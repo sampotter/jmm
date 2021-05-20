e97f62f45faf98a9f8c09a43f86701beaaa69173
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-05-20 16:19:42 -0400

    make sure point source is in the interior of the domain

--- examples/testing_multiple_arrivals.py
+++ examples/testing_multiple_arrivals.py
@@ -7,9 +7,7 @@ from jmm.mesh import Mesh3
 from jmm.multiple_arrivals import Domain, PointSourceField, MultipleArrivals
 
 logging.basicConfig(level=logging.INFO)
-
-src_index = 0
-num_arrivals = 5
+log = logging.getLogger('testing_multiple_arrivals.py')
 
 surf_grid = pv.read(f'sethian_shadow/Building.obj')
 grid = pv.read(f'sethian_shadow/Building.vtu')
@@ -23,6 +21,11 @@ num_dom_cells = info['num_dom_cells']
 num_dom_verts = info['num_dom_points']
 
 domain = Domain(extended_verts, extended_cells, num_dom_verts, num_dom_cells)
+
+src_index = next(l for l in range(num_dom_verts) if not domain.mesh.bdv(l))
+num_arrivals = 5
+
+log.info('computing %d arrivals starting from index %d', num_arrivals, src_index)
 
 field = PointSourceField(domain, src_index)
 
