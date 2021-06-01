aff371e2aae412c67d855088ae63ea8a9db7b41e
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-06-01 19:26:29 -0400

    update testing_multiple_arrivals.py

--- examples/testing_multiple_arrivals.py
+++ examples/testing_multiple_arrivals.py
@@ -1,10 +1,13 @@
+import colorcet as cc
 import logging
 import json
 import numpy as np
 import pyvista as pv
+import pyvistaqt as pvqt
 
 from jmm.mesh import Mesh3
-from jmm.multiple_arrivals import Domain, PointSourceField, MultipleArrivals
+from jmm.multiple_arrivals import Domain, Field, PointSourceField, MultipleArrivals
+from jmm.plot import *
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger('testing_multiple_arrivals.py')
@@ -20,7 +23,10 @@ extended_verts = grid.points.astype(np.float64)
 num_dom_cells = info['num_dom_cells']
 num_dom_verts = info['num_dom_points']
 
-domain = Domain(extended_verts, extended_cells, num_dom_verts, num_dom_cells)
+verts = extended_verts[:num_dom_verts]
+cells = extended_cells[:num_dom_cells]
+
+domain = Domain(verts, cells, refl_coef=0.7)
 
 src_index = next(l for l in range(num_dom_verts) if not domain.mesh.bdv(l))
 num_arrivals = 5
@@ -29,4 +35,22 @@ log.info('computing %d arrivals starting from index %d', num_arrivals, src_index
 
 field = PointSourceField(domain, src_index)
 
-multiple_arrivals = MultipleArrivals(domain, field, num_arrivals)
+ma = MultipleArrivals(domain, field, num_arrivals)
+ma.traverse()
+
+############################################################################
+
+f = ma._fields[2]
+
+poly = pv.PolyData(domain.mesh.verts)
+poly['time'] = f.time
+poly['scale'] = f._scale
+poly['amplitude'] = 20*np.log10(np.clip(f.amplitude, 1e-3, 1))
+
+_ = pvqt.BackgroundPlotter()
+plot_mesh2(_, domain.mesh.get_surface_mesh(), opacity=0.25, color='white')
+plot_field_BCs(_, f, opacity=0.4, r=domain.h, show_edges=True, color='magenta')
+# _.add_mesh(poly, scalars='time', cmap=cc.cm.rainbow)
+# _.add_mesh(poly, scalars='amplitude', cmap=cc.cm.fire)
+_.add_mesh(poly, scalars='scale', cmap=cc.cm.gray)
+s
