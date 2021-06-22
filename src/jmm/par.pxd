aa1a8c2c28262f91df209cbe80ef8dfdaae0b9e1
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-06-22 10:09:56 -0400

    wrap par3_num_active and par3_get_active

--- src/jmm/par.pxd
+++ src/jmm/par.pxd
@@ -5,6 +5,8 @@ cdef extern from "par.h":
         size_t l[3]
         dbl b[3]
     size_t par3_size(const par3 *par)
+    size_t par3_num_active(const par3 *par)
+    void par3_get_active(const par3 *par, size_t *l, dbl *b)
 
 cdef class Parent3:
     cdef par3 par
--- src/jmm/par.pyx
+++ src/jmm/par.pyx
@@ -21,3 +21,14 @@ cdef class Parent3:
         cdef dbl[:] b = np.empty((self.size,), dtype=np.float64)
         memcpy(&b[0], self.par.b, self.size*sizeof(dbl))
         return np.asarray(b)
+
+    @property
+    def num_active(self):
+        return par3_num_active(&self.par)
+
+    def get_active(self):
+        cdef size_t num_active = self.num_active
+        cdef size_t[::1] l = np.empty(num_active, dtype=np.uintp)
+        cdef dbl[::1] b = np.empty(num_active, dtype=np.float64)
+        par3_get_active(&self.par, &l[0], &b[0])
+        return np.asarray(l), np.asarray(b)
