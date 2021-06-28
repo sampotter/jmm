4a83c10ae9bb97344dd892e2145dc3a04f1a3258
Samuel F. Potter <sfp@umiacs.umd.edu>
2021-06-28 13:04:41 -0400

    use e x nu instead of nu for reflector support

--- src/utri.c
+++ src/utri.c
@@ -622,12 +622,6 @@ bool utri_accept_refl_BCs_update(utri_s const *u, eik3_s const *eik) {
   if (!eik3_get_refl_bdf_inc_on_diff_edge(eik, le, lf))
     return true;
 
-  /* Get the face normal for the reflector and negate it, since we
-   * want to use it as a support vector for the wedge. */
-  dbl nu[3];
-  mesh3_get_face_normal(mesh, lf, nu);
-  dbl3_negate(nu);
-
   /* Get the edge's tangent and midpoint and the centroid of the
    * reflecting face. */
   dbl e[3]; mesh3_get_edge_tangent(mesh, le, e);
@@ -644,17 +638,40 @@ bool utri_accept_refl_BCs_update(utri_s const *u, eik3_s const *eik) {
   dbl3_normalize(t);
 
   dbl t_dot_xm = dbl3_dot(t, xm);
-  dbl dot = dbl3_dot(t, xf) - t_dot_xm;
-  assert(dot != 0);
-  if (dot > 0)
-    dbl3_negate(t);
+  {
+    dbl dot = dbl3_dot(t, xf) - t_dot_xm;
+    assert(dot != 0);
+    if (dot > 0)
+      dbl3_negate(t);
+  }
+
+  /* Next, get the unit vector that supports the part of the wedge
+   * that isn't coplanar with the reflector, denote it `b` */
+
+  /* Get the face normal for the reflector and negate it, since we
+   * want to use it as a support vector for the wedge. */
+  dbl nu[3];
+  mesh3_get_face_normal(mesh, lf, nu);
+  dbl3_negate(nu);
+
+  dbl b[3];
+  dbl3_cross(e, nu, b);
+  dbl3_normalize(b);
+
+  dbl b_dot_xm = dbl3_dot(b, xm);
+  {
+    dbl dot = dbl3_dot(b, xf) - b_dot_xm;
+    assert(dot != 0);
+    if (dot > 0)
+      dbl3_negate(b);
+  }
 
   // return whether l is outside of this wedge
 
   dbl const *x = mesh3_get_vert_ptr(mesh, l);
 
   return dbl3_dot(t, x) - t_dot_xm > 0 ||
-    dbl3_dot(nu, x) - dbl3_dot(nu, xm) > 0;
+    dbl3_dot(b, x) - dbl3_dot(b, xm) > 0;
 }
 
 static dbl get_lambda(utri_s const *u) {
