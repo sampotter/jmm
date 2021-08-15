25a6af9b3d7318604d336e3103ce71a612445889
Sam Potter <sfp@debian>
2021-08-15 12:56:03 -0400

    add a test for dbl44_dbl4_mul and dbl4_dbl44_mul

--- test/test_dbl44.c
+++ test/test_dbl44.c
@@ -123,3 +123,32 @@ Ensure (dbl44, dbl4_solve_works) {
 
   gsl_rng_free(rng);
 }
+
+Ensure (dbl44, dbl4_mul_works) {
+  dbl44 A = {
+    {0.04440129622672145, -1.791725852886146, -0.5465298382048241, -0.8416259191899776},
+    {-1.287411706678259, -1.774436226554693, 0.9067743971259874, -0.4538715589706994},
+    {-2.658977126008932, -0.4894769507574523, 0.2398244941599099, 0.5530479398676308},
+    {-1.664763207458297, 0.496870525703022, -1.210463986309432, -1.215804454782426}
+  };
+
+  dbl4 b = {-1.428737780331591, 0.05818709668635221, 0.2934308272841706, 0.03704906574396089};
+  dbl4 x_gt = {-0.359243291423951, 1.985384496175262, 3.861361643515079, 2.007189675043842};
+  dbl4 y_gt = {-0.9802523382021779, 2.331438087912691, 0.8591357373548978, 1.293290174714141};
+
+  dbl4 x;
+  dbl44_dbl4_mul(A, b, x);
+
+  assert_that_double(x[0], is_nearly_double(x_gt[0]));
+  assert_that_double(x[1], is_nearly_double(x_gt[1]));
+  assert_that_double(x[2], is_nearly_double(x_gt[2]));
+  assert_that_double(x[3], is_nearly_double(x_gt[3]));
+
+  dbl4 y;
+  dbl4_dbl44_mul(b, A, y);
+
+  assert_that_double(y[0], is_nearly_double(y_gt[0]));
+  assert_that_double(y[1], is_nearly_double(y_gt[1]));
+  assert_that_double(y[2], is_nearly_double(y_gt[2]));
+  assert_that_double(y[3], is_nearly_double(y_gt[3]));
+}
