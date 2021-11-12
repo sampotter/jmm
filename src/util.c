97625800e14825b50ea8231bfe5918749146f905
Samuel F. Potter <sfp@cims.nyu.edu>
2021-11-12 11:40:46 -0500

    fixes

--- src/util.c
+++ src/util.c
@@ -66,7 +66,7 @@ dbl shrink(dbl x, dbl eps) {
 
 bool contains(void const *arr, size_t len, void const *elt, size_t size) {
   char *ptr = (char *)arr;
-  for (int i = 0; i < len; ++i) {
+  for (size_t i = 0; i < len; ++i) {
     if (!memcmp((void *)(ptr + size*i), elt, size)) {
       return true;
     }
