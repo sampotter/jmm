60163394909074ef334cf0ff186e7e9dc315077b
Samuel F. Potter <sfp@cims.nyu.edu>
2022-08-31 13:50:24 -0400

    refactoring simple_building example

---
+++ examples/simple_building/render.c
@@ -0,0 +1,416 @@
+#include <assert.h>
+#include <argp.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <jmm/array.h>
+#include <jmm/bmesh.h>
+#include <jmm/camera.h>
+#include <jmm/mesh2.h>
+#include <jmm/mesh3.h>
+#include <jmm/rtree.h>
+#include <jmm/util.h>
+
+const char *argp_program_version = "render 0.0";
+char const *argp_program_bug_address = "sfp@cims.nyu.edu";
+
+static char doc[] = ""; // TODO: add some docs here when it makes sense
+
+static char args_doc[] = "BRANCH_PATH ...";
+
+enum long_opts {
+  LONG_OPT_T0 = 777,
+  LONG_OPT_T1,
+  LONG_OPT_FRAMES_PER_METER,
+  LONG_OPT_VERTS_PATH,
+  LONG_OPT_CELLS_PATH,
+};
+
+static struct argp_option options[] = {
+  {"verbose", 'v', 0, OPTION_ARG_OPTIONAL, "Verbose output", 0},
+  {"speed", 'c', "SPEED", OPTION_ARG_OPTIONAL,
+   "Speed of sound in m/s (default: 340.3 m/s)", 0},
+  {"t0", LONG_OPT_T0, "T0", OPTION_ARG_OPTIONAL,
+   "Initial time point for rendering", 0},
+  {"t1", LONG_OPT_T1, "T1", OPTION_ARG_OPTIONAL,
+   "Final time point for rendering", 0},
+  {"frames_per_meter", LONG_OPT_FRAMES_PER_METER, "FRAMES_PER_METER",
+   OPTION_ARG_OPTIONAL,
+   "Rendering frame rate (default: 23.98 s^-1)", 0},
+  {"verts_path", LONG_OPT_VERTS_PATH, "VERTS_PATH", OPTION_ARG_OPTIONAL,
+   "Path to binary file with vertices (default: \"verts.bin\")", 0},
+  {"cells_path", LONG_OPT_CELLS_PATH, "CELLS_PATH", OPTION_ARG_OPTIONAL,
+   "Path to binary file with vertices (default: \"cells.bin\")", 0},
+  {0}
+};
+
+typedef struct render_spec {
+  bool verbose;
+  dbl c;
+  dbl t0;
+  dbl t1;
+  dbl frames_per_meter;
+  char *verts_path;
+  char *cells_path;
+  array_s *branch_path_arr;
+} render_spec_s;
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+  render_spec_s *spec = state->input;
+  size_t n;
+  char *branch_path;
+  switch (key) {
+  case 'v':
+    spec->verbose = true;
+    break;
+  case 'c':
+    spec->c = atof(arg);
+    break;
+  case LONG_OPT_T0:
+    spec->t0 = atof(arg);
+    break;
+  case LONG_OPT_T1:
+    spec->t1 = atof(arg);
+    break;
+  case LONG_OPT_FRAMES_PER_METER:
+    spec->frames_per_meter = atof(arg);
+    break;
+  case LONG_OPT_VERTS_PATH:
+    n = strlen(arg);
+    spec->verts_path = malloc(n + 1);
+    strncpy(spec->verts_path, arg, n + 1);
+    break;
+  case LONG_OPT_CELLS_PATH:
+    n = strlen(arg);
+    spec->cells_path = malloc(n + 1);
+    strncpy(spec->cells_path, arg, n + 1);
+    break;
+  case ARGP_KEY_ARG:
+    n = strlen(arg);
+    branch_path = malloc(n + 1);
+    strncpy(branch_path, arg, n + 1);
+    array_append(spec->branch_path_arr, &branch_path);
+    break;
+  case ARGP_KEY_END:
+    if (array_is_empty(spec->branch_path_arr))
+      argp_usage(state);
+    break;
+  default:
+    return ARGP_ERR_UNKNOWN;
+  }
+  return EXIT_SUCCESS;
+}
+
+static struct argp argp = {options, parse_opt, args_doc, doc, 0, 0, 0};
+
+int main(int argc, char *argv[]) {
+  int code = EXIT_SUCCESS;
+
+  render_spec_s spec = {
+    .verbose = false,
+    .c = 340.3, /* m/s */
+    .t0 = NAN,
+    .t1 = NAN,
+    .frames_per_meter = 23.98,
+    .verts_path = NULL,
+    .cells_path = NULL
+  };
+
+  array_alloc(&spec.branch_path_arr);
+  array_init(spec.branch_path_arr, sizeof(char *), ARRAY_DEFAULT_CAPACITY);
+
+  argp_parse(&argp, argc, argv, 0, 0, &spec);
+
+  if (spec.verbose) {
+    printf("Render info:\n");
+    printf("- t0 = %g\n", spec.t0);
+    printf("- t1 = %g\n", spec.t1);
+    printf("- frames_per_meter = %g\n", spec.frames_per_meter);
+    printf("- verts_path = %s\n", spec.verts_path);
+    printf("- cells_path = %s\n", spec.cells_path);
+    printf("- branch_paths:\n");
+    for (size_t i = 0; i < array_size(spec.branch_path_arr); ++i) {
+      char *branch_path;
+      array_get(spec.branch_path_arr, i, &branch_path);
+      printf("  + %s\n", branch_path);
+    }
+  }
+
+  mesh3_data_s data;
+  mesh3_data_init_from_bin(
+    &data,
+    spec.verts_path ? spec.verts_path : "verts.bin",
+    spec.cells_path ? spec.cells_path : "cells.bin");
+
+  dbl eps = 1e-5;
+
+  mesh3_s *mesh;
+  mesh3_alloc(&mesh);
+  mesh3_init(mesh, &data, true, &eps);
+
+  array_s *bmesh_arr;
+  array_alloc(&bmesh_arr);
+  array_init(bmesh_arr, sizeof(bmesh33_s *), ARRAY_DEFAULT_CAPACITY);
+
+  array_s *spread_arr;
+  array_alloc(&spread_arr);
+  array_init(spread_arr, sizeof(dbl *), ARRAY_DEFAULT_CAPACITY);
+
+  array_s *org_arr;
+  array_alloc(&org_arr);
+  array_init(org_arr, sizeof(dbl *), ARRAY_DEFAULT_CAPACITY);
+
+  for (size_t i = 0; i < array_size(spec.branch_path_arr); ++i) {
+    char path[256];
+
+    char *branch_path;
+    array_get(spec.branch_path_arr, i, &branch_path);
+
+    sprintf(path, "%s/jet.bin", branch_path);
+
+    FILE *fp = fopen(path, "r");
+    jet31t *jet = malloc(mesh3_nverts(mesh)*sizeof(jet31t));
+    fread(jet, sizeof(jet31t), mesh3_nverts(mesh), fp);
+    fclose(fp);
+
+    bmesh33_s *bmesh;
+    bmesh33_alloc(&bmesh);
+    bmesh33_init_from_mesh3_and_jets(bmesh, mesh, jet);
+
+    array_append(bmesh_arr, &bmesh);
+
+    free(jet);
+
+    sprintf(path, "%s/spread.bin", branch_path);
+    fp = fopen(path, "r");
+    dbl *spread = malloc(mesh3_nverts(mesh)*sizeof(dbl));
+    fread(spread, sizeof(dbl), mesh3_nverts(mesh), fp);
+    fclose(fp);
+    array_append(spread_arr, &spread);
+
+    sprintf(path, "%s/org.bin", branch_path);
+    fp = fopen(path, "r");
+    dbl *org = malloc(mesh3_nverts(mesh)*sizeof(dbl));
+    fread(org, sizeof(dbl), mesh3_nverts(mesh), fp);
+    fclose(fp);
+    array_append(org_arr, &org);
+  }
+
+  mesh2_s *surface_mesh = mesh3_get_surface_mesh(mesh);
+
+  /* TODO: read camera from file */
+  camera_s camera = {
+    // .type = CAMERA_TYPE_ORTHOGRAPHIC,
+    .type = CAMERA_TYPE_PERSPECTIVE,
+    .pos = {-8, 8, 20},
+    .look = {0, 0, -1},
+    .left = {-1, 0, 0},
+    .up = {0, 1, 0},
+    // .width = 22.0,
+    // .height = 22.0/4,
+    .fovy = 30,
+    .aspect = 1,
+    .dim = {512, 512}
+  };
+
+  dbl tau0 = spec.c*spec.t0;
+  dbl tau1 = spec.c*spec.t1;
+
+  size_t num_frames = floor(spec.frames_per_meter*(tau1 - tau0));
+  if (spec.verbose)
+    printf("Rendering %lu frames\n", num_frames);
+
+  dbl *tau = malloc(num_frames*sizeof(dbl));
+  for (size_t i = 0; i < num_frames; ++i)
+    tau[i] = tau0 + i/spec.frames_per_meter;
+
+  for (size_t i = 0; i < num_frames; ++i) {
+    if (spec.verbose)
+      printf("frame %lu/%lu (tau = %g m)\n", i + 1, num_frames, tau[i]);
+
+    rtree_s *rtree;
+    rtree_alloc(&rtree);
+    rtree_init(rtree, 16, RTREE_SPLIT_STRATEGY_SURFACE_AREA);
+
+    rtree_insert_mesh2(rtree, surface_mesh);
+
+    array_s *level_bmesh_arr;
+    array_alloc(&level_bmesh_arr);
+    array_init(level_bmesh_arr, sizeof(bmesh33_s *), ARRAY_DEFAULT_CAPACITY);
+
+    for (size_t j = 0; j < array_size(bmesh_arr); ++j) {
+      bmesh33_s *bmesh;
+      array_get(bmesh_arr, j, &bmesh);
+      bmesh33_s *level_bmesh = bmesh33_restrict_to_level(bmesh, tau[i]);
+      rtree_insert_bmesh33(rtree, level_bmesh);
+      array_append(level_bmesh_arr, &level_bmesh);
+    }
+
+    rtree_build(rtree);
+
+    size_t npix = camera.dim[0]*camera.dim[1];
+
+    dbl4 *img = malloc(npix*sizeof(dbl4));
+
+    dbl3 surf_rgb = {0.54, 0.54, 0.54};
+    dbl3 eik_rgb = {1.0, 1.0, 1.0};
+
+    dbl surf_alpha = 0.5;
+    dbl eik_alpha = 1;
+
+    for (size_t i = 0, l = 0; i < camera.dim[0]; ++i) {
+      for (size_t j = 0; j < camera.dim[1]; ++j, ++l) {
+        ray3 ray = camera_get_ray_for_index(&camera, i, j);
+
+        isect isect;
+        rtree_intersect(rtree, &ray, &isect, NULL);
+
+        img[l][0] = 0;
+        img[l][1] = 0;
+        img[l][2] = 0;
+        img[l][3] = isfinite(isect.t) ? 1 : 0;
+
+        dbl transparency = 1;
+        dbl const *rgb = NULL;
+        dbl3 n;
+
+        while (isfinite(isect.t)) {
+          robj_type_e robj_type = robj_get_type(isect.obj);
+          void const *robj_data = robj_get_data(isect.obj);
+
+          dbl alpha = 1, scale = 1;
+
+          /* Increment the distance along the ray */
+          dbl3_saxpy_inplace(isect.t, ray.dir, ray.org);
+
+          /* Update the current alpha and RGB value */
+          switch (robj_type) {
+          case ROBJ_MESH2_TRI:
+            alpha *= surf_alpha;
+            rgb = &surf_rgb[0];
+            break;
+          case ROBJ_BMESH33_CELL:
+            alpha *= eik_alpha;
+            rgb = &eik_rgb[0];
+            break;
+          default:
+            assert(false);
+          }
+
+          if (robj_type == ROBJ_BMESH33_CELL) {
+            bmesh33_cell_s const *bmesh33_cell = robj_data;
+            dbl spread_interp, org_interp;
+
+            /* Figure out which level mesh this cell belongs to :-( */
+            size_t k;
+            for (k = 0; k < array_size(level_bmesh_arr); ++k) {
+              bmesh33_s *level_bmesh;
+              array_get(level_bmesh_arr, k, &level_bmesh);
+              if (bmesh33_cell->bmesh == level_bmesh)
+                break;
+            }
+            assert(k < array_size(level_bmesh_arr));
+
+            dbl const *spread;
+            array_get(spread_arr, k, &spread);
+            spread_interp = mesh3_linterp(mesh, spread, ray.org);
+
+            dbl const *org;
+            array_get(org_arr, k, &org);
+            org_interp = mesh3_linterp(mesh, org, ray.org);
+
+            // Convert the interpolated spreading factor to dB
+            dbl spread_dB = 20*log10(fmax(1e-16, spread_interp));
+            /* Clamp and map the range [-60 dB, 0 dB] to [0, 1] for
+             * use as a scaling factor */
+            dbl spread_mapped = fmax(0, fmin(1, 1 - spread_dB/(-90)));
+            alpha *= spread_mapped*squash(org_interp, 2);
+          }
+
+          /* Get the surface normal and dot it with the eye vector for
+           * Lambertian shading */
+          switch (robj_type) {
+          case ROBJ_MESH2_TRI:
+            mesh2_tri_s const *mesh2_tri = robj_data;
+            mesh2_get_unit_surface_normal(surface_mesh, mesh2_tri->l, n);
+            break;
+          case ROBJ_BMESH33_CELL:
+            bmesh33_cell_s const *bmesh33_cell = robj_data;
+            bmesh33_cell_Df(bmesh33_cell, ray.org, n);
+            dbl3_normalize(n);
+            break;
+          default:
+            assert(false);
+          }
+          scale *= fabs(dbl3_dot(n, ray.dir));
+
+          /* We're raymarching, so do backwards alpha blending */
+          dbl3_saxpy_inplace(scale*alpha, rgb, img[l]);
+
+          /* Update transparency for early stopping */
+          transparency *= 1 - alpha;
+          if (transparency < 1e-3)
+            break;
+
+          /* Advance the start of the ray and keep tracing.
+           *
+           * NOTE: if we have multiple overlapping intersections, we
+           * might trip them repeatedly, so we need to skip any
+           * intersections with a distance of zero here. */
+          rtree_intersect(rtree, &ray, &isect, isect.obj);
+          while (isect.t < EPS) {
+            dbl3_saxpy_inplace(EPS, ray.dir, ray.org);
+            rtree_intersect(rtree, &ray, &isect, isect.obj);
+          }
+        }
+      }
+    }
+
+    char filename[128];
+    snprintf(filename, 128, "image%04lu.bin", i);
+
+    FILE *fp = fopen(filename, "wb");
+    fwrite(img, sizeof(dbl4), npix, fp);
+    fclose(fp);
+
+    for (size_t i = 0; i < array_size(level_bmesh_arr); ++i) {
+      bmesh33_s *level_bmesh;
+      array_get(level_bmesh_arr, i, &level_bmesh);
+      bmesh33_deinit(level_bmesh);
+      bmesh33_dealloc(&level_bmesh);
+    }
+    array_deinit(level_bmesh_arr);
+    array_dealloc(&level_bmesh_arr);
+
+    rtree_deinit(rtree);
+    rtree_dealloc(&rtree);
+  }
+
+  mesh2_deinit(surface_mesh);
+  mesh2_dealloc(&surface_mesh);
+
+  for (size_t i = 0; i < array_size(bmesh_arr); ++i) {
+    bmesh33_s *bmesh;
+    array_get(bmesh_arr, i, &bmesh);
+    bmesh33_deinit(bmesh);
+    bmesh33_dealloc(&bmesh);
+  }
+  array_deinit(bmesh_arr);
+  array_dealloc(&bmesh_arr);
+
+  if (spec.verts_path != NULL) free(spec.verts_path);
+  if (spec.cells_path != NULL) free(spec.cells_path);
+
+  for (size_t i = 0; i < array_size(spec.branch_path_arr); ++i) {
+    char *branch_path;
+    array_get(spec.branch_path_arr, i, &branch_path);
+    free(branch_path);
+  }
+  array_deinit(spec.branch_path_arr);
+  array_dealloc(&spec.branch_path_arr);
+
+  free(tau);
+
+  return code;
+}
--- examples/simple_building/simulate.c
+++ examples/simple_building/simulate.c
@@ -3,32 +3,24 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-#include <jmm/bmesh.h>
-#include <jmm/camera.h>
 #include <jmm/eik3.h>
 #include <jmm/eik3hh.h>
 #include <jmm/eik3hh_branch.h>
 #include <jmm/util.h>
 
-const char *argp_program_version = "simple_building 0.0";
+const char *argp_program_version = "simulate 0.0";
 char const *argp_program_bug_address = "sfp@cims.nyu.edu";
 
 static char doc[] = ""; // TODO: add some docs here when it makes sense
 
 static char args_doc[] = "EXAMPLE";
 
-enum long_opts {
-  LONG_OPT_RENDER = 777,
-  LONG_OPT_T0,
-  LONG_OPT_T1,
-  LONG_OPT_FRAMERATE
-};
-
 static struct argp_option options[] = {
   {"verbose", 'v', 0, OPTION_ARG_OPTIONAL, "Produce verbose output", 0},
-  {"maxvol", 'a', "VOLUME", OPTION_ARG_OPTIONAL,
-   "Maximum tetrahedron volume (default: 0.01)", 0},
   {"xsrc", 'x', "POINT", OPTION_ARG_OPTIONAL,
    "Point source location (format: \"x,y,z\")", 0},
   {"Rcoef", 'R', "RCOEF", OPTION_ARG_OPTIONAL,
@@ -40,29 +32,15 @@ static struct argp_option options[] = {
   {"freq", 'f', "FREQUENCY", OPTION_ARG_OPTIONAL,
    "Angular frequency of wave (default: 1000)", 0},
   {"omega", 0, "FREQUENCY", OPTION_ALIAS, 0, 0},
-  {"render", LONG_OPT_RENDER, 0, OPTION_ARG_OPTIONAL,
-   "Render video of wavefront", 0},
-  {"t0", LONG_OPT_T0, "T0", OPTION_ARG_OPTIONAL,
-   "Initial time point for rendering", 0},
-  {"t1", LONG_OPT_T1, "T1", OPTION_ARG_OPTIONAL,
-   "Final time point for rendering", 0},
-  {"framerate", LONG_OPT_FRAMERATE, "FRAMERATE", OPTION_ARG_OPTIONAL,
-   "Rendering frame rate (default: 23.98 s^-1)", 0},
   {0}
 };
 
 typedef struct problem_spec {
   bool verbose;
-  dbl maxvol;
   dbl3 xsrc;
   dbl rfac;
   dbl c;
   dbl omega;
-  bool render;
-  dbl t0;
-  dbl t1;
-  dbl frame_rate;
-  char *off_path;
 } problem_spec_s;
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
@@ -71,16 +49,13 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
   case 'v':
     spec->verbose = true;
     break;
-  case 'a':
-    spec->maxvol = atof(arg);
-    break;
-  case 'x':
+  case 'x': {
     char *str = arg;
     spec->xsrc[0] = atof(strtok_r(str, ",", &str));
     spec->xsrc[1] = atof(strtok_r(str, ",", &str));
     spec->xsrc[2] = atof(strtok_r(str, ",", &str));
     break;
-  case 'r':
+  } case 'r':
     spec->rfac = atof(arg);
     break;
   case 'c':
@@ -88,27 +63,6 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
     break;
   case 'f':
     spec->omega = atof(arg);
-    break;
-  case LONG_OPT_RENDER:
-    spec->render = true;
-    break;
-  case LONG_OPT_T0:
-    spec->t0 = atof(arg);
-    break;
-  case LONG_OPT_T1:
-    spec->t1 = atof(arg);
-    break;
-  case LONG_OPT_FRAMERATE:
-    spec->frame_rate = atof(arg);
-    break;
-  case ARGP_KEY_ARG:
-    size_t n = strlen(arg);
-    spec->off_path = malloc(n + 1);
-    strncpy(spec->off_path, arg, n + 1);
-    break;
-  case ARGP_KEY_END:
-    if (spec->off_path == NULL)
-      argp_usage(state);
     break;
   default:
     return ARGP_ERR_UNKNOWN;
@@ -118,41 +72,23 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
 
 static struct argp argp = {options, parse_opt, args_doc, doc, 0, 0, 0};
 
-void save_bin_files_and_slices(problem_spec_s const *spec,
-                               eik3hh_branch_s const *branch,
-                               grid2_to_mesh3_mapping_s const *mapping,
-                               char const *name) {
+void save_bin_files(problem_spec_s const *spec,
+                    eik3hh_branch_s const *branch,
+                    char const *name) {
   char path[256];
 
-  sprintf(path, "%s_jet.bin", name);
+  struct stat st = {0};
+  if (stat(name, &st) == -1)
+    mkdir(name, 0700);
+
+  sprintf(path, "%s/jet.bin", name);
   eik3hh_branch_dump_jet(branch, path);
 
-  sprintf(path, "%s_org.bin", name);
+  sprintf(path, "%s/org.bin", name);
   eik3hh_branch_dump_org(branch, path);
 
-  sprintf(path, "%s_spread.bin", name);
+  sprintf(path, "%s/spread.bin", name);
   eik3hh_branch_dump_spread(branch, path);
-
-  /* Save T slice to disk */
-  toc();
-  sprintf(path, "%s_T_slice.bin", name);
-  eik3hh_branch_dump_xy_slice(branch, mapping, FIELD_T, path);
-  printf("Saved xy-slice (z = %g) of the direct eikonal [%1.2gs]\n",
-         spec->xsrc[2], toc());
-
-  /* Save spreading factor slice to disk */
-  toc();
-  sprintf(path, "%s_spread_slice.bin", name);
-  eik3hh_branch_dump_xy_slice(branch, mapping, FIELD_SPREADING, path);
-  printf("Saved xy-slice (z = %g) of the direct spreading [%1.2gs]\n",
-         spec->xsrc[2], toc());
-
-  /* Save origin slice to disk */
-  toc();
-  sprintf(path, "%s_origin_slice.bin", name);
-  eik3hh_branch_dump_xy_slice(branch, mapping, FIELD_ORIGIN, path);
-  printf("Saved xy-slice (z = %g) of the direct origin [%1.2gs]\n",
-         spec->xsrc[2], toc());
 }
 
 int main(int argc, char *argv[]) {
@@ -160,27 +96,19 @@ int main(int argc, char *argv[]) {
 
   problem_spec_s spec = {
     .verbose = false,
-    .maxvol = 0.01,
     .xsrc = {NAN, NAN, NAN},
     .rfac = 0.1,
     .c = 340.3,
     .omega = 100,
-    .render = false,
-    .t0 = NAN,
-    .t1 = NAN,
-    .frame_rate = 23.98,
-    .off_path = NULL
   };
 
   argp_parse(&argp, argc, argv, 0, 0, &spec);
 
   if (spec.verbose) {
     printf("Problem info:\n");
-    printf("- maxvol: %g m^3\n", spec.maxvol);
-    printf("- xsrc: (%g, %g, %g) m\n", spec.xsrc[0], spec.xsrc[1], spec.xsrc[2]);
+    printf("- xsrc: (%g, %g, %g) m\n",spec.xsrc[0],spec.xsrc[1],spec.xsrc[2]);
     printf("- rfac: %g m\n", spec.rfac);
     printf("- omega: %g Hz\n", spec.omega);
-    printf("- off_path: %s\n", spec.off_path);
   }
 
   toc();
@@ -188,7 +116,7 @@ int main(int argc, char *argv[]) {
   dbl eps = 1e-5;
 
   mesh3_data_s data;
-  mesh3_data_from_off_file(&data, spec.off_path, spec.maxvol, spec.verbose);
+  mesh3_data_init_from_bin(&data, "verts.bin", "cells.bin");
   mesh3_data_insert_vert(&data, spec.xsrc, eps);
 
   mesh3_s *mesh;
@@ -198,44 +126,10 @@ int main(int argc, char *argv[]) {
   mesh3_dump_verts(mesh, "verts.bin");
   mesh3_dump_cells(mesh, "cells.bin");
 
-  rect3 bbox;
-  mesh3_get_bbox(mesh, &bbox);
-
-  if (spec.verbose) {
-    printf("Built tetrahedron mesh [%1.2gs]:\n", toc());
-    printf("- %lu vertices\n", mesh3_nverts(mesh));
-    printf("- %lu cells.\n", mesh3_ncells(mesh));
-    printf("- bounding box: [%g, %g] x [%g, %g] x [%g, %g]\n",
-           bbox.min[0], bbox.max[0],
-           bbox.min[1], bbox.max[1],
-           bbox.min[2], bbox.max[2]);
-    printf("- h: %g\n", mesh3_get_mean_edge_length(mesh));
-    printf("- wrote mesh vertices to verts.bin\n");
-    printf("- wrote mesh tetrahedra to cells.bins\n");
-  }
-
   if (!mesh3_contains_ball(mesh, spec.xsrc, spec.rfac)) {
     fprintf(stderr, "ERROR: mesh doesn't fully contain factoring ball\n");
     code = EXIT_FAILURE;
     goto cleanup;
-  }
-
-  /* Set up the image grid for making 2D plots */
-  size_t ngrid = 1024 + 1;
-  grid2_s img_grid = {
-    .shape = {ngrid, ngrid},
-    .xymin = {bbox.min[0], bbox.min[1]},
-    .h = (bbox.max[0] - bbox.min[0])/(ngrid - 1),
-    .order = ORDER_ROW_MAJOR
-  };
-  grid2_save(&img_grid, "img_grid.txt");
-
-  /* Set up the mapping from the image grid to the tetrahedron mesh. */
-  toc();
-  grid2_to_mesh3_mapping_s mapping;
-  grid2_to_mesh3_mapping_init_xy(&mapping, &img_grid, mesh, spec.xsrc[2]);
-  if (spec.verbose) {
-    printf("Set up grid-to-mesh mapping [%1.2gs]\n", toc());
   }
 
   /* Set up the Helmholtz data structure to manage the different
@@ -250,7 +144,7 @@ int main(int argc, char *argv[]) {
   eik3hh_branch_s *root_branch = eik3hh_get_root_branch(hh);
   eik3hh_branch_solve(root_branch, spec.verbose);
 
-  save_bin_files_and_slices(&spec, root_branch, &mapping, "direct");
+  save_bin_files(&spec, root_branch, "direct");
 
   /* Find visible reflecting boundaries */
   array_s *refl_inds = eik3hh_branch_get_visible_refls(root_branch);
@@ -267,35 +161,13 @@ int main(int argc, char *argv[]) {
 
     char name[128];
     sprintf(name, "refl%03d", (int)refl_ind);
-    save_bin_files_and_slices(&spec, refl_branch, &mapping, name);
+    save_bin_files(&spec, refl_branch, name);
   }
 
   array_deinit(refl_inds);
   array_dealloc(&refl_inds);
 
-  /** Render frames to disk */
-  if (spec.render) {
-    /* TODO: read camera from file */
-    camera_s camera = {
-      // .type = CAMERA_TYPE_ORTHOGRAPHIC,
-      .type = CAMERA_TYPE_PERSPECTIVE,
-      .pos = {0, 0, 42},
-      .look = {0, 0, -1},
-      .left = {-1, 0, 0},
-      .up = {0, 1, 0},
-      // .width = 22.0,
-      // .height = 22.0/4,
-      .fovy = 30,
-      .aspect = 1,
-      .dim = {256, 256}
-    };
-    eik3hh_branch_render_frames(
-      root_branch, &camera, spec.t0, spec.t1, spec.frame_rate, true);
-  }
-
 cleanup:
-
-  grid2_to_mesh3_mapping_deinit(&mapping);
 
   eik3hh_deinit(hh);
   eik3hh_dealloc(&hh);
@@ -303,10 +175,6 @@ cleanup:
   /* Free tetrahedron mesh */
   mesh3_deinit(mesh);
   mesh3_dealloc(&mesh);
-
-  /* Free stuff related to problem specification */
-  if (spec.off_path)
-    free(spec.off_path);
 
   return code;
 }
