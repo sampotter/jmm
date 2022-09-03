4701535032d820f4d87bc40bbeb36e98d8286ee9
Samuel F. Potter <sfp@cims.nyu.edu>
2022-09-03 14:43:49 -0400

    update itd example

--- examples/itd/itd.c
+++ examples/itd/itd.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <jmm/bmesh.h>
 #include <jmm/eik3.h>
 #include <jmm/mesh3.h>
 
@@ -5,36 +9,149 @@ int main(void) {
   dbl eps = 1e-5;
   dbl maxvol = 8e5;
   bool verbose = true;
-  dbl3 xsrc = {-0.9e3, -0.9e3, -0.9e3};
-  dbl rfac = 100;
+  dbl c = 340.3e3; // mm/s
+
+  size_t num_el = 256;
+  size_t num_az = 2*num_el;
+
+  /* NOTE: units in mm! */
+
+  dbl rfac = 4; // mm
+  dbl3 xsrc_R = {0.0, -69.910568, 0.0}; // right ear
+  dbl3 xsrc_L = {0.0, 69.821861, 0.0}; // left ear
+
+  dbl fliege_grid_r = 500; // mm
+
+  char const *itd_bin_path = "itd.bin";
+
+  /* Load Fliege grid points */
+  char const *fliege_grid_txt_path = "fliege64.txt";
+  char const *fliege_grid_bin_path = "fliege64.bin";
+
+  array_s *fliege_grid_arr;
+  array_alloc(&fliege_grid_arr);
+  array_init(fliege_grid_arr, sizeof(dbl3), ARRAY_DEFAULT_CAPACITY);
+
+  /* Read CSV text file containing Fliege grid node elevation (first
+   * column) and azimuth (second column) */
+  FILE *fp = fopen(fliege_grid_txt_path, "r");
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  while ((read = getline(&line, &len, fp)) != -1) {
+    dbl el, az;
+    char *saveptr, *pc;
+    pc = strtok_r(line, " ", &saveptr);
+    el = atof(pc);
+    pc = strtok_r(NULL, " ", &saveptr);
+    az = atof(pc);
+    dbl3 point = {cos(az)*sin(el), sin(az)*sin(el), cos(el)};
+    dbl3_dbl_mul_inplace(point, fliege_grid_r);
+    array_append(fliege_grid_arr, &point);
+  }
+  fclose(fp);
+
+  /* Dump Cartesian coordinates of Fliege grid to a binary file */
+  fp = fopen(fliege_grid_bin_path, "wb");
+  for (size_t i = 0; i < array_size(fliege_grid_arr); ++i) {
+    dbl3 point;
+    array_get(fliege_grid_arr, i, &point);
+    fwrite(point, sizeof(dbl3), 1, fp);
+  }
+  fclose(fp);
 
   /* Units are in mm */
-  char const *off_path = "HUTUB_pp2_in_cube_50k.off";
+  char const *off_path = "HUTUB_pp2_in_cube_50k_origin_fixed.off";
 
   mesh3_data_s data;
   mesh3_data_init_from_off_file(&data, off_path, maxvol, verbose);
 
-  mesh3_data_insert_vert(&data, xsrc, eps);
+  /* Add the point sources to the mesh */
+  mesh3_data_insert_vert(&data, xsrc_R, eps);
+  mesh3_data_insert_vert(&data, xsrc_L, eps);
 
+  /* Set up tetrahedron mesh */
   mesh3_s *mesh;
   mesh3_alloc(&mesh);
   mesh3_init(mesh, &data, true, &eps);
 
+  /* Write vertices and cells to disk in row-major order */
   mesh3_dump_verts(mesh, "verts.bin");
   mesh3_dump_cells(mesh, "cells.bin");
 
-  eik3_s *eik;
-  eik3_alloc(&eik);
-  eik3_init(eik, mesh);
+  /* Solve the eikonal equation for the left ear */
+  eik3_s *eik_L;
+  eik3_alloc(&eik_L);
+  eik3_init(eik_L, mesh);
+  eik3_add_pt_src_bcs(eik_L, xsrc_L, rfac);
+  eik3_solve(eik_L);
+  eik3_dump_jet(eik_L, "jet_L.bin");
 
-  eik3_add_pt_src_bcs(eik, xsrc, rfac);
-  eik3_solve(eik);
+  /* Solve the eikonal equation for the right ear */
+  eik3_s *eik_R;
+  eik3_alloc(&eik_R);
+  eik3_init(eik_R, mesh);
+  eik3_add_pt_src_bcs(eik_R, xsrc_R, rfac);
+  eik3_solve(eik_R);
+  eik3_dump_jet(eik_R, "jet_R.bin");
 
-  eik3_deinit(eik);
-  eik3_dealloc(&eik);
+  jet31t const *jet_L = eik3_get_jet_ptr(eik_L);
+  jet31t const *jet_R = eik3_get_jet_ptr(eik_R);
+
+  /* Set up tetrahedral spline interpolating jet data for left ear */
+  bmesh33_s *tau_L;
+  bmesh33_alloc(&tau_L);
+  bmesh33_init_from_mesh3_and_jets(tau_L, mesh, jet_L);
+
+  /* Set up tetrahedral spline interpolating jet data for right ear */
+  bmesh33_s *tau_R;
+  bmesh33_alloc(&tau_R);
+  bmesh33_init_from_mesh3_and_jets(tau_R, mesh, jet_R);
+
+  /* Compute ITD at Fliege grid nodes and save to disk */
+  fp = fopen(itd_bin_path, "wb");
+  for (size_t i = 0; i < array_size(fliege_grid_arr); ++i) {
+    dbl3 point;
+    array_get(fliege_grid_arr, i, &point);
+    dbl T_L = bmesh33_f(tau_L, point)/c;
+    dbl T_R = bmesh33_f(tau_R, point)/c;
+    dbl itd = T_R - T_L;
+    fwrite(&itd, sizeof(dbl), 1, fp);
+  }
+  fclose(fp);
+
+  fp = fopen("itd_uniform.bin", "wb");
+  for (size_t i = 0; i < num_el; ++i) {
+    dbl el = JMM_PI*i/(dbl)num_el;
+    for (size_t j = 0; j < num_az; ++j) {
+      dbl az = 2*JMM_PI*j/(dbl)num_az;
+      dbl3 point = {cos(az)*sin(el), sin(az)*sin(el), cos(el)};
+      dbl3_dbl_mul_inplace(point, fliege_grid_r);
+      dbl T_L = bmesh33_f(tau_L, point)/c;
+      dbl T_R = bmesh33_f(tau_R, point)/c;
+      dbl itd = T_R - T_L;
+      fwrite(&itd, sizeof(dbl), 1, fp);
+    }
+  }
+  fclose(fp);
+
+  bmesh33_deinit(tau_R);
+  bmesh33_dealloc(&tau_R);
+
+  bmesh33_deinit(tau_L);
+  bmesh33_dealloc(&tau_L);
+
+  eik3_deinit(eik_L);
+  eik3_dealloc(&eik_L);
+
+  eik3_deinit(eik_R);
+  eik3_dealloc(&eik_R);
 
   mesh3_deinit(mesh);
   mesh3_dealloc(&mesh);
 
   mesh3_data_deinit(&data);
+
+  array_deinit(fliege_grid_arr);
+  array_dealloc(&fliege_grid_arr);
 }
--- examples/itd/scratch.py
+++ examples/itd/scratch.py
@@ -1,9 +1,13 @@
+import colorcet as cc
 import pyvista as pv
 import pyvistaqt as pvqt
+import matplotlib.pyplot as plt
 import numpy as np
 import vtk
 
 from pathlib import Path
+
+plt.ion()
 
 itd_build_dir = Path('../../builddir/examples/itd')
 
@@ -28,14 +32,123 @@ grid_tet = pv.UnstructuredGrid({vtk.VTK_TETRA: C_tet}, V_tet)
 
 P_tet = grid_tet.cell_centers().points
 
-cell_ind = (P_tet[:, 1] > 400).nonzero()[0]
-subgrid = grid_tet.extract_cells(cell_ind)
+cell_ind = (P_tet[:, 1] < 0).nonzero()[0]
+# subgrid = grid_tet.extract_cells(cell_ind)
 
-bbox = pv.Box((-1e3, 1e3, -1e3, 1e3, -1e3, 1e3))
+bbox = pv.Box(grid_tet.bounds)
+
+# plotter = pvqt.BackgroundPlotter()
+# # plotter.add_mesh(subgrid, 'lightgrey', lighting=True, show_edges=True)
+# plotter.add_mesh(grid_tet, style='wireframe')
+# plotter.add_mesh(pv.Sphere(4, (V_tet[-1, 0], V_tet[-1, 1], V_tet[-1, 2])), color='red', opacity=0.5)
+# # plotter.add_mesh(bbox, 'r', 'wireframe')
+# # plotter.add_legend([[' Input Mesh ', 'r'],
+# #                     [' Tessellated Mesh ', 'black']])
+# plotter.show()
+
+jet_L = np.fromfile(itd_build_dir/'jet_L.bin').reshape(-1, 4)
+Tau_L = jet_L[:, 0]
+GradTau_L = jet_L[:, 1:]
+
+jet_R = np.fromfile(itd_build_dir/'jet_R.bin').reshape(-1, 4)
+Tau_R = jet_R[:, 0]
+GradTau_R = jet_R[:, 1:]
+
+c = 340.3e3
+
+T_L = Tau_L/c
+T_R = Tau_R/c
+itd = T_R - T_L
+itd_abs_max = abs(itd).max()
+itd_contours = np.linspace(-0.5e-3, 0.5e-3, 11)
+
+grid_tet['T_L'] = T_L
+grid_tet['T_R'] = T_R
+grid_tet['itd'] = itd
+
+bust = pv.read('HUTUB_pp2_in_cube_50k_origin_fixed_bust_only_BROKEN.off')
+
+clipped = grid_tet.clip('z', origin=(0, 0, 0))
+contours = clipped.contour(scalars='T_L')
+plotter = pvqt.BackgroundPlotter()
+plotter.add_mesh(bbox, 'r', 'wireframe')
+plotter.add_mesh(contours)
+plotter.add_mesh(bust)
+
+clipped = grid_tet.clip('z', origin=(0, 0, 0))
+contours = clipped.contour(scalars='T_R')
+plotter = pvqt.BackgroundPlotter()
+plotter.add_mesh(bbox, 'r', 'wireframe')
+plotter.add_mesh(contours)
+plotter.add_mesh(bust)
+
+clipped = grid_tet.clip('z', origin=(0, 0, 0))
+contours = clipped.contour(itd_contours, scalars='itd')
+plotter = pvqt.BackgroundPlotter()
+plotter.add_mesh(bbox, 'r', 'wireframe')
+plotter.add_mesh(contours)
+plotter.add_mesh(bust)
+
+El, Az = [], []
+with open('fliege64.txt', 'r') as f:
+    for line in f:
+        el, az = line.strip().split()
+        El.append(float(el))
+        Az.append(float(az))
+El, Az = np.array(El), np.array(Az)
+
+r = 500
+x = r*np.cos(Az)*np.sin(El)
+y = r*np.sin(Az)*np.sin(El)
+z = r*np.cos(El)
+
+X_fliege = np.fromfile(itd_build_dir/'fliege64.bin').reshape(-1, 3)
+grid_fliege = pv.PolyData(X_fliege)
+
+itd_fliege = np.fromfile(itd_build_dir/'itd.bin')
+grid_fliege['itd'] = itd_fliege
 
 plotter = pvqt.BackgroundPlotter()
-plotter.add_mesh(subgrid, 'lightgrey', lighting=True, show_edges=True)
-plotter.add_mesh(bbox, 'r', 'wireframe')
-plotter.add_legend([[' Input Mesh ', 'r'],
-                    [' Tessellated Mesh ', 'black']])
-plotter.show()
+plotter.add_mesh(bust)
+plotter.add_mesh(grid_fliege)
+
+plt.figure(figsize=(8, 4))
+plt.scatter(Az, El, s=15, c=itd_fliege, cmap=cc.cm.gouldian)
+plt.colorbar()
+plt.xlim(-np.pi, np.pi)
+plt.ylim(0, np.pi)
+plt.title('ITD [s]')
+plt.xlabel('Azimuth')
+plt.ylabel('Elevation')
+plt.gca().set_aspect('equal')
+plt.tight_layout()
+plt.savefig('itd.png')
+
+m_uniform = 256
+n_uniform = 512
+
+itd_uniform = np.fromfile(itd_build_dir/'itd_uniform.bin').reshape(
+    m_uniform, n_uniform)
+
+itd_uniform_abs_max = np.nanmax(abs(itd_uniform))
+itd_uniform_levels = np.linspace(-itd_uniform_abs_max, itd_uniform_abs_max, 21)
+
+Az_uniform, El_uniform = np.meshgrid(
+    np.linspace(0, 2*np.pi, n_uniform),
+    np.linspace(0, np.pi, m_uniform),
+)
+
+plt.figure()
+# plt.imshow(itd_uniform, extent=[0, 2*np.pi, 0, np.pi], cmap=cc.cm.blues)
+plt.contourf(Az_uniform, El_uniform, itd_uniform, itd_uniform_levels,
+             cmap=cc.cm.gouldian)
+plt.xticks(np.linspace(0, 2*np.pi, 9, endpoint=True))
+plt.yticks(np.linspace(0, np.pi, 5, endpoint=True))
+plt.gca().invert_yaxis()
+plt.gca().set_yticklabels(['0', r'$\pi/4$', r'$\pi/2$', r'$3\pi/4$', r'$\pi$'])
+plt.gca().set_xticklabels(['0', r'$\pi/4$', r'$\pi/2$', r'$3\pi/4$',
+                           r'$\pi$', r'$5\pi/4$', r'$3\pi/2$', r'$7\pi/4$',
+                           r'$2\pi$'])
+plt.colorbar()
+plt.gca().set_aspect('equal')
+plt.show()
